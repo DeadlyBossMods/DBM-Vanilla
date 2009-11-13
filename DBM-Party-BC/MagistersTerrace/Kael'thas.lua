0c3a61a8e66b1116397b71ee30694d2443c6cacb
Adam <MysticalOS@users.noreply.github.com>
2009-11-13 02:31:17 +0000

    Start phoenix timer if not heroic. was in wrong place, oops.

--- DBM-Party-BC/MagistersTerrace/Kael'thas.lua
+++ DBM-Party-BC/MagistersTerrace/Kael'thas.lua
@@ -24,9 +24,9 @@ local timerPhoenix      = mod:NewNextTimer(45, 44194)--Best guess based on limit
 local specwarnPyroblast = mod:NewSpecialWarning("specwarnPyroblast")
 
 function mod:OnCombatStart(delay)
+    timerPhoenix:Start(10-delay)
 	if mod:IsDifficulty("heroic5") then
         timerShockBarrior:Start(-delay)
-        timerPhoenix:Start(10-delay)
     end
 end
 
