be1382046cf39de994ad7c2ab3e440ff58d891c3
Arta <Arta@d0860a8b-7ba9-43f0-818a-d428a84863f8>
2010-01-11 16:54:21 +0000

    First alpha versions of BWL mods

--- DBM-BWL/Ebonroc.lua
+++ DBM-BWL/Ebonroc.lua
@@ -6,4 +6,35 @@ mod:SetCreatureID(14601)
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
+	"SPELL_CAST_START",
+	"SPELL_AURA_APPLIED"
 )
+
+local warnWingBuffet	= mod:NewCastAnnounce(23339)
+local warnShadowFlame	= mod:NewCastAnnounce(22539)
+local warnShadow		= mod:NewTargetAnnounce(23340)
+
+local timerWingBuffet	= mod:NewNextTimer(31, 23339)
+local timerShadowFlame	= mod:NewCastTimer(2, 22539)
+local timerShadow		= mod:NewTargetTimer(8, 23340)
+
+function mod:OnCombatStart(delay)
+	timerWingBuffet:Start(-delay)
+end
+
+function mod:SPELL_CAST_START(args)
+	if args:IsSpellID(23339) and self:IsInCombat() then
+		warnWingBuffet:Show()
+		timerWingBuffet:Start()
+	elseif args:IsSpellID(22539) and self:IsInCombat() then
+		timerShadowFlame:Start()
+		warnShadowFlame:Show()
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(23340) then
+		warnShadow:Show(args.destName)
+		timerShadow:Start(args.destName)
+	end
+end
--- DBM-BWL/Razorgore.lua
+++ DBM-BWL/Razorgore.lua
@@ -6,4 +6,21 @@ mod:SetCreatureID(12435)
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
+	"SPELL_AURA_APPLIED"
 )
+
+local warnConflagration		= mod:NewTargetAnnounce(23023)
+
+local timerConflagration	= mod:NewTargetTimer(10, 23023)
+local timerAddsSpawn		= mod:NewTimer(45, "TimerAddsSpawn")
+
+function mod:OnCombatStart(delay)
+	timerAddsSpawn:Start()
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(23023) then
+		warnConflagration:Show(args.destName)
+		timerConflagration:Start(args.destName)
+	end
+end
