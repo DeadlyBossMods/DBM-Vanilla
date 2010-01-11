ea0a81bb2e648598d26c2c5542238525ae2631d4
Arta <Arta@d0860a8b-7ba9-43f0-818a-d428a84863f8>
2010-01-11 16:54:21 +0000

    First alpha versions of BWL mods

--- trunk/DBM-BWL/Broodlord.lua
+++ trunk/DBM-BWL/Broodlord.lua
@@ -6,4 +6,30 @@ mod:SetCreatureID(12017)
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
+	"SPELL_CAST_SUCCESS",
+	"SPELL_AURA_APPLIED"
 )
+
+local warnBlastWave	= mod:NewSpellAnnounce(23331)
+local warnKnockAway	= mod:NewSpellAnnounce(18670)
+local warnMortal	= mod:NewTargetAnnounce(23573)
+
+local timerMortal	= mod:NewTargetTimer(5, 23573)
+
+function mod:OnCombatStart(delay)
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args:IsSpellID(23331) and self:IsInCombat() then
+		warnBlastWave:Show()
+	elseif args:IsSpellID(18670) and self:IsInCombat() then
+		warnKnockAway:Show()
+	end
+end
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(23573) and self:IsInCombat() then
+		warnMortal:Show(args.destName)
+		timerMortal:Start(args.destName)
+	end
+end
--- trunk/DBM-BWL/localization.en.lua
+++ trunk/DBM-BWL/localization.en.lua
@@ -8,6 +8,12 @@ L = DBM:GetModLocalization("Razorgore")
 L:SetGeneralLocalization{
 	name = "Razorgore the Untamed"
 }
+L:SetTimerLocalization{
+	TimerAddsSpawn	= "Adds spawning"
+}
+L:SetOptionLocalization{
+	TimerAddsSpawn	= "Show timer for first adds spawning"
+}
 
 -------------------
 --  Vaelastrasz  --
@@ -62,6 +68,20 @@ L = DBM:GetModLocalization("Chromaggus")
 L:SetGeneralLocalization{
 	name = "Chromaggus"
 }
+L:SetWarningLocalization{
+	WarnBreathSoon	= "Breath soon",
+	WarnBreath		= "%s",
+	WarnPhase2Soon	= "Phase 2 soon"
+}
+L:SetTimerLocalization{
+	TimerBreathCD	= "%s CD"
+}
+L:SetOptionLocalization{
+	WarnBreathSoon	= "Show pre-warning for Chromaggus' Breaths",
+	WarnBreath		= "Show warning when Chromaggus casts one of his Breaths",
+	TimerBreathCD	= "Show Breath CD",
+	WarnPhase2Soon	= "Announce when phase 2 is about to start"
+}
 
 ----------------
 --  Nefarian  --
@@ -70,4 +90,34 @@ L = DBM:GetModLocalization("Nefarian")
 
 L:SetGeneralLocalization{
 	name = "Nefarian"
+}
+L:SetWarningLocalization{
+	WarnClassCallSoon	= "Class call soon",
+	WarnClassCall		= "%s call",
+	WarnPhaseSoon		= "Phase %s soon",
+	WarnPhase			= "Phase %s"
+}
+L:SetTimerLocalization{
+	TimerClassCall		= "%s call"
+}
+L:SetOptionLocalization{
+	TimerClassCall		= "Show timer for class call duration",
+	WarnClassCallSoon	= "Pre-warn class calls",
+	WarnClassCall		= "Announce class calls",
+	WarnPhaseSoon		= "Announce when a new phase is about to start",
+	WarnPhase			= "Announce phase change"
+}
+L:SetMiscLocalization{
+	YellPull	= "Let the games begin!",
+	YellP2		= "Well done, my minions. The mortals' courage begins to wane! Now, let's see how they contend with the true Lord of Blackrock Spire!!!",
+	YellP3		= "Impossible! Rise my minions!  Serve your master once more!",
+	YellShaman	= "Shamans, show me",
+	YellPaladin	= "Paladins... I've heard you have many lives. Show me.",
+	YellDruid	= "Druids and your silly shapeshifting. Lets see it in action!",
+	YellPriest	= "Priests! If you're going to keep healing like that, we might as well make it a little more interesting!",
+	YellWarrior	= "Warriors, I know you can hit harder than that! Lets see it!",
+	YellRogue	= "Rogues? Stop hiding and face me!",
+	YellWarlock	= "Warlocks, you shouldn't be playing with magic you don't understand. See what happens?",
+	YellHunter	= "Hunters and your annoying pea-shooters!",
+	YellMage	= "Mages too? You should be more careful when you play with magic..."
 }
