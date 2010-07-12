bccc32fcc80dc0d490551afbd4d929a99625b079
Adam <MysticalOS@users.noreply.github.com>
2010-07-12 20:04:01 +0000

    Fix three bugs fear stuff to not be spammy in larger size raids

--- DBM-AQ40/ThreeBugs.lua
+++ DBM-AQ40/ThreeBugs.lua
@@ -7,7 +7,6 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
 	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED",
 	"SPELL_CAST_START"
 )
 mod:SetBossHealthInfo(
@@ -16,10 +15,10 @@ mod:SetBossHealthInfo(
 	15511, L.Kri
 )
 
-local warnFear	= mod:NewTargetAnnounce(26580, 2)
+local warnFear	= mod:NewSpellAnnounce(26580, 2)
 local warnHeal	= mod:NewCastAnnounce(25807, 3)
 
-local timerFear	= mod:NewTargetTimer(8, 26580)
+local timerFear	= mod:NewBuffActiveTimer(8, 26580)
 local timerHeal	= mod:NewCastTimer(2, 25807)
 
 function mod:OnCombatStart(delay)
@@ -27,14 +26,8 @@ end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(26580) then
-		warnFear:Show(args.destName)
-		timerFear:Start(args.destName)
-	end
-end
-
-function mod:SPELL_AURA_REMOVED(args)
-	if args:IsSpellID(26580) then
-		timerFear:Cancel(args.destName)
+		warnFear:Show()
+		timerFear:Start()
 	end
 end
 
