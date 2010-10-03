8218257dc1a5b40c4c4d53902286439c458d2610
Adam <MysticalOS@users.noreply.github.com>
2010-10-03 02:03:19 +0000

    BWL update. Nefarian special warning for mind control (on by default for tanks to get control of person). Chromagus bronze debuffs redone so it announces the cast vs announcing EVERY debuff.

--- DBM-BWL/Chromaggus.lua
+++ DBM-BWL/Chromaggus.lua
@@ -7,21 +7,21 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
 	"SPELL_CAST_START",
-	"SPELL_CAST_SUCCESS",
 	"SPELL_AURA_APPLIED",
+--	"SPELL_AURA_REFRESH",
 	"SPELL_AURA_REMOVED",
 	"UNIT_HEALTH"
 )
 
 local warnBreathSoon	= mod:NewAnnounce("WarnBreathSoon", 1, 23316)
 local warnBreath		= mod:NewAnnounce("WarnBreath", 2, 23316)
-local warnRed			= mod:NewTargetAnnounce(23155, 2, nil, false)
-local warnGreen			= mod:NewTargetAnnounce(23169, 2, nil, false)
-local warnBlue			= mod:NewTargetAnnounce(23153, 2, nil, false)
-local warnBlack			= mod:NewTargetAnnounce(23154, 2, nil, false)
-local warnBronze		= mod:NewSpellAnnounce(23170, 2)
+local warnRed			= mod:NewSpellAnnounce(23155, 2, nil, false)
+local warnGreen			= mod:NewSpellAnnounce(23169, 2, nil, false)
+local warnBlue			= mod:NewSpellAnnounce(23153, 2, nil, false)
+local warnBlack			= mod:NewSpellAnnounce(23154, 2, nil, false)
+local warnBronze		= mod:NewSpellAnnounce(23170, 4)
 local warnEnrage		= mod:NewSpellAnnounce(23128)
-local warnPhase2Soon	= mod:NewAnnounce("WarnPhase2Soon")
+local warnPhase2Soon	= mod:NewAnnounce("WarnPhase2Soon", 1, "Interface\\Icons\\Spell_Nature_WispSplode")
 local warnPhase2		= mod:NewPhaseAnnounce(2)
 
 local specWarnBronze	= mod:NewSpecialWarningYou(23170)
@@ -30,13 +30,23 @@ local timerBreath		= mod:NewTimer(2, "TimerBreath")
 local timerBreathCD		= mod:NewTimer(60, "TimerBreathCD")
 local timerEnrage		= mod:NewBuffActiveTimer(8, 23128)
 
-
 local prewarn_P2
+local lastred = 0
+local lastgreen = 0
+local lastblue = 0
+local lastblack = 0
+local lastbronze = 0
+
 function mod:OnCombatStart(delay)
 	warnBreathSoon:Schedule(25-delay)
 	timerBreathCD:Start(30-delay, "Breath 1")
 	timerBreathCD:Start(-delay, "Breath 2")
-	prewarn_P2 = false;
+	prewarn_P2 = false
+	lastred = 0
+	lastgreen = 0
+	lastblue = 0
+	lastblack = 0
+	lastbronze = 0
 end
 
 function mod:SPELL_CAST_START(args)
@@ -48,25 +58,26 @@ function mod:SPELL_CAST_START(args)
 	end
 end
 
-function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(23170) then
-		warnBronze:Show()
-	end
-end
-
 function mod:SPELL_AURA_APPLIED(args)
-	if args:IsSpellID(23155) then
-		warnRed:Show(args.destName)
-	elseif args:IsSpellID(23169) then
-		warnGreen:Show(args.destName)
-	elseif args:IsSpellID(23153) then
-		warnBlue:Show(args.destName)
-	elseif args:IsSpellID(23154) then
-		warnBlack:Show(args.destName)
+	if args:IsSpellID(23155) and GetTime() - lastred > 3 then
+		warnRed:Show()
+		lastred = GetTime()
+	elseif args:IsSpellID(23169) and GetTime() - lastgreen > 3 then
+		warnGreen:Show()
+		lastgreen = GetTime()
+	elseif args:IsSpellID(23153) and GetTime() - lastblue > 3 then
+		warnBlue:Show()
+		lastblue = GetTime()
+	elseif args:IsSpellID(23154) and GetTime() - lastblack > 3 then
+		warnBlack:Show()
+		lastblack = GetTime()
 	elseif args:IsSpellID(23170) then
-		warnBronze:Show()
 		if args:IsPlayer() then
 			specWarnBronze:Show()
+		end
+		if GetTime() - lastbronze > 3 
+			warnBronze:Show()
+			lastbronze = GetTime()
 		end
 	elseif args:IsSpellID(23128) then
 		warnEnrage:Show()
@@ -75,6 +86,9 @@ function mod:SPELL_AURA_APPLIED(args)
 		warnPhase2:Show()
 	end
 end
+
+--Possibly needed hard to say. 
+--mod.SPELL_AURA_REFRESH = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
 	if args:IsSpellID(23128) then
