6d12996d404ad4a24f4518ca132d058ed6bbfc25
Omega <Omega@d0860a8b-7ba9-43f0-818a-d428a84863f8>
2010-11-16 08:02:14 +0000

    Redo razorgore mod to use hacked kill detection that only considers it a kill if razorgore dies AFTER all the eggs are destroyed. Untested

--- trunk/DBM-BWL/Razorgore.lua
+++ trunk/DBM-BWL/Razorgore.lua
@@ -2,23 +2,30 @@ local mod	= DBM:NewMod("Razorgore", "DBM-BWL", 1)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(12435)
+mod:SetCreatureID(12435, 99999)--Bogus detection to prevent invalid kill detection if razorgore happens to die in phase 1
 mod:SetMinSyncRevision(168)
-mod:RegisterCombat("yell", L.YellPull)--Will fail if msg find isn't used, msg match won't find yell since a line break is omitted
+mod:RegisterCombat("yell", L.YellPull)
 mod:SetWipeTime(45)--guesswork
 
 mod:RegisterEvents(
 	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED"
+	"SPELL_AURA_REMOVED",
+	"CHAT_MSG_RAID_BOSS_EMOTE",
+	"CHAT_MSG_MONSTER_EMOTE",
+	"UNIT_DIED"
 )
 
+local warnPhase2			= mod:NewPhaseAnnounce(2)
 local warnConflagration		= mod:NewTargetAnnounce(23023)
 
 local timerConflagration	= mod:NewTargetTimer(10, 23023)
 local timerAddsSpawn		= mod:NewTimer(47, "TimerAddsSpawn", 19879)--Only for start of adds, not adds after the adds heh.
 
+local phase2 = false
+
 function mod:OnCombatStart(delay)
 	timerAddsSpawn:Start()
+	phase2 = false
 end
 
 function mod:SPELL_AURA_APPLIED(args)
@@ -31,5 +38,21 @@ end
 function mod:SPELL_AURA_REMOVED(args)
 	if args:IsSpellID(23023) then
 		timerConflagration:Start(args.destName)
+	end
+end
+
+function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
+	if msg == L.Phase2Emote or msg:find(L.Phase2Emote) then
+		warnPhase2:Show()
+		phase2 = true
+	end
+end
+
+mod.CHAT_MSG_RAID_BOSS_EMOTE = mod.CHAT_MSG_MONSTER_EMOTE -- Not sure which is used yet so for time being support both.
+
+function mod:UNIT_DIED(args)
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 12435 and phase2 then--Only trigger kill for unit_died if he dies in phase 2, otherwise it's an auto wipe.
+		DBM:EndCombat(self)
 	end
 end
