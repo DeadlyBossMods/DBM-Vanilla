92479030b071d5c6f6f90de67e550180951b221d
Adam <MysticalOS@users.noreply.github.com>
2010-11-16 08:02:14 +0000

    Redo razorgore mod to use hacked kill detection that only considers it a kill if razorgore dies AFTER all the eggs are destroyed. Untested

--- DBM-BWL/localization.en.lua
+++ DBM-BWL/localization.en.lua
@@ -15,7 +15,8 @@ L:SetOptionLocalization{
 	TimerAddsSpawn	= "Show timer for first adds spawning"
 }
 L:SetMiscLocalization{
-	YellPull = "Intruders have breached the hatchery! Sound the alarm! Protect the eggs at all costs!\r\n"--Yes this yell actually has a return and new line in it. as grabbed by transcriptor
+	Phase2Emote	= "Orb of Domination loses power and shuts off!",
+	YellPull 	= "Intruders have breached the hatchery! Sound the alarm! Protect the eggs at all costs!\r\n"--Yes this yell actually has a return and new line in it. as grabbed by transcriptor
 }
 -------------------
 --  Vaelastrasz  --
