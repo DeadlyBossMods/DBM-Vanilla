7c9dc81956fd868f346c0803a1fe4d7eee20bdfd
Omega <Omega@d0860a8b-7ba9-43f0-818a-d428a84863f8>
2010-11-23 14:43:53 +0000

    update build numbers in mapsizes checks to the 4.0.3a final release build as the maps weren't included in any of the other 4.x releases

--- trunk/DBM-AQ20/MapSizes.lua
+++ trunk/DBM-AQ20/MapSizes.lua
@@ -1,5 +1,5 @@
 local WowBuild = select(2, GetBuildInfo())
-if tonumber(WowBuild) < 13165 then return end
+if tonumber(WowBuild) < 13329 then return end
 
 DBM:RegisterMapSize("RuinsofAhnQiraj",
 	1, 2512.49987792969, 1675.0
--- trunk/DBM-ZG/MapSizes.lua
+++ trunk/DBM-ZG/MapSizes.lua
@@ -1,5 +1,5 @@
 --[[local WowBuild = select(2, GetBuildInfo())
-if tonumber(WowBuild) < 13165 then return end
+if tonumber(WowBuild) < 13329 then return end
 
 DBM:RegisterMapSize("ZulGurub",
 	1, 1877.08325195312, 1252.0830078125
