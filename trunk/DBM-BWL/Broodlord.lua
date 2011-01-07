a95ba304833b4aa019f714877f8ee9c0976e558d
Omega <Omega@d0860a8b-7ba9-43f0-818a-d428a84863f8>
2011-01-07 19:22:48 +0000

    Switch broodlord's pull method to INSTANCE_ENCOUNTER_ENGAGE_UNIT (untested, but at worse this will just break a mod for a boss that was barely working to begin with using player regen :)

--- trunk/DBM-BWL/Broodlord.lua
+++ trunk/DBM-BWL/Broodlord.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(12017)
-mod:RegisterCombat("combat")
+mod:RegisterCombat("engage")
 
 mod:RegisterEvents(
 	"SPELL_CAST_SUCCESS",
