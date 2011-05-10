36fa71cdd39cd0ff16b6e7923977e93546b8e180
Adam <MysticalOS@users.noreply.github.com>
2011-05-10 21:53:30 +0000

    Kelthuzads model doesn't render, what so ever, for some reason cthun doesn't scale, so bo th of these are commented out

--- DBM-Naxx/FrostwyrmLair/Kel'Thuzad.lua
+++ DBM-Naxx/FrostwyrmLair/Kel'Thuzad.lua
@@ -3,13 +3,11 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 4548 $"):sub(12, -3))
 mod:SetCreatureID(15990)
-mod:SetModelID(15945)
+--mod:SetModelID(15945)--Doesn't work at all, doesn't even render.
 mod:SetMinCombatTime(60)
 mod:SetUsedIcons(1, 2, 3, 4, 5, 6, 7, 8)
 
 mod:RegisterCombat("yell", L.Yell)
-
-mod:EnableModel()
 
 mod:RegisterEvents(
 	"SPELL_AURA_APPLIED",
