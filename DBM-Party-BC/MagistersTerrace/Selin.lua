0eb2f60e300fafe8829647b8d3b08256d6d1797a
arta <arta@d0860a8b-7ba9-43f0-818a-d428a84863f8>
2011-05-13 15:54:33 +0000

    and Magister's Terrace done as well :)

--- DBM-Party-BC/MagistersTerrace/Selin.lua
+++ DBM-Party-BC/MagistersTerrace/Selin.lua
@@ -4,6 +4,8 @@ local L = mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 
 mod:SetCreatureID(24723)
+mod:SetModelID(22642)
+
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
