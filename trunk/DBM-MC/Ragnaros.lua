3e52ca53b3f963a192710d00aa6e2aba2ed8c8e2
Omega <Omega@d0860a8b-7ba9-43f0-818a-d428a84863f8>
2011-05-18 20:16:11 +0000

    old rag should now correctly detect early emerge when all 8 adds die

--- trunk/DBM-MC/Ragnaros.lua
+++ trunk/DBM-MC/Ragnaros.lua
@@ -11,65 +11,71 @@ mod:RegisterEvents(
 	"SPELL_CAST_SUCCESS",
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_REMOVED",
-	"CHAT_MSG_MONSTER_YELL"
+	"CHAT_MSG_MONSTER_YELL",
+	"UNIT_DIED"
 )
 
 local warnWrathRag		= mod:NewSpellAnnounce(20566)
---local warnHandRag		= mod:NewSpellAnnounce(19780)--does not show in combat log. need transciptor to get more data on this later
 local warnSubmergeSoon	= mod:NewAnnounce("WarnSubmergeSoon", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendBurrow.blp")
 local warnSubmerge		= mod:NewAnnounce("WarnSubmerge", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendBurrow.blp")
 local warnEmergeSoon	= mod:NewAnnounce("WarnEmergeSoon", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendUnBurrow.blp")
 local warnEmerge		= mod:NewAnnounce("WarnEmerge", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendUnBurrow.blp")
 
 local timerWrathRag		= mod:NewNextTimer(30, 20566)
---local timerHandRag		= mod:NewNextTimer(111, 19780)
 local timerSubmerge		= mod:NewTimer(180, "TimerSubmerge", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendBurrow.blp")
 local timerEmerge		= mod:NewTimer(90, "TimerEmerge", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendUnBurrow.blp")
 local timerCombatStart	= mod:NewTimer(78, "TimerCombatStart", 2457)
 
-local submerged
+local addDied = 0
+
 function mod:OnCombatStart(delay)
-	submerged = false
+	addDied = 0
 	timerSubmerge:Start(-delay)
 	warnSubmergeSoon:Schedule(170-delay)
 	timerWrathRag:Start(27-delay)
 end
 
-function mod:emerged()
+local function emerged()
 	timerEmerge:Cancel()
 	warnEmerge:Show()
 	timerSubmerge:Start()
 	warnSubmergeSoon:Schedule(170)
-	submerge = false
+	addDied = 0
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	if args:IsSpellID(20566) then
 		warnWrathRag:Show()
 		timerWrathRag:Start()
---	elseif args:IsSpellID(19780) then
---		warnHandRag:Show()
 	end
 end
 
 function mod:CHAT_MSG_MONSTER_YELL(msg)
 	if msg == L.Submerge then
 		self:SendSync("Submerge")
-	elseif msg ~= L.Submerge and submerged then
-		self:SendSync("Emerge")
 	elseif msg == L.Pull then
 		timerCombatStart:Start()
 	end
 end
 
-function mod:OnSync(msg, arg)
-	if msg == "Submerge" and not submerge then
-		submerge = true
+function mod:OnSync(msg)
+	if msg == "Submerge" then
+		self:Unschedule(emerged)
+		warnEmergeSoon:Cancel()
 		warnSubmerge:Show()
 		timerEmerge:Start()
 		warnEmergeSoon:Schedule(80)
-		self:ScheduleMethod(90, "emerged")
-	elseif msg == "Emerge" then
-		emerged()
+		self:Schedule(90, emerged)
+	end
+end
+
+function mod:UNIT_DIED(args)
+	local cid = self:GetCIDFromGUID(args.destGUID)
+	if cid == 12143 then--Son of Flame
+		addDied = addDied + 1
+		if addDied == 8 then--After all 8 die he emerges immediately
+			self:Unschedule(emerged)
+			emerged()
+		end
 	end
 end
