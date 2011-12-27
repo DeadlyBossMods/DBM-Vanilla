ae4161bd2272344866612839acbfab69f66a7802
Omega <Omega@d0860a8b-7ba9-43f0-818a-d428a84863f8>
2011-12-27 06:15:46 +0000

    another malygos update, even more useless locals removed.

--- trunk/DBM-EyeOfEternity/Malygos.lua
+++ trunk/DBM-EyeOfEternity/Malygos.lua
@@ -19,21 +19,23 @@ mod:RegisterEvents(
 local warnSpark				= mod:NewSpellAnnounce(56140, 2, 59381)
 local warnVortex			= mod:NewSpellAnnounce(56105, 3)
 local warnVortexSoon		= mod:NewSoonAnnounce(56105, 2)
-local warnBreathInc			= mod:NewAnnounce("WarningBreathSoon", 3, 60072)
-local warnBreath			= mod:NewAnnounce("WarningBreath", 4, 60072)
+local warnBreathInc			= mod:NewSoonAnnounce(56505, 3)
+local warnBreath			= mod:NewSpellAnnounce(56505, 4)
 local warnSurge				= mod:NewTargetAnnounce(60936, 3)
 local warnStaticField		= mod:NewSpellAnnounce(57430, 3)
 
+local specWarnBreath		= mod:NewSpecialWarningSpell(56505, nil, nil, nil, true)
 local specWarnSurge			= mod:NewSpecialWarningYou(60936)
 
-local enrageTimer			= mod:NewBerserkTimer(615)
 local timerSpark			= mod:NewNextTimer(30, 56140, nil, nil, nil, 59381)
-local timerArcaneBreathCD	= mod:NewCDTimer(30, 56272)--Don't know CD, need a log he doesn't die too fast in.
 local timerVortex			= mod:NewCastTimer(11, 56105)
 local timerVortexCD			= mod:NewNextTimer(60, 56105)
-local timerBreath			= mod:NewTimer(59, "TimerBreath", 60072)
-local timerStaticFieldCD	= mod:NewCDTimer(15.5, 57430)
+local timerBreath			= mod:NewBuffActiveTimer(8, 56505)--lasts 5 seconds plus 3 sec cast.
+local timerBreathCD			= mod:NewCDTimer(59, 56505)
+local timerStaticFieldCD	= mod:NewCDTimer(15.5, 57430)--High 15-25 second variatoin
 local timerAchieve      	= mod:NewAchievementTimer(360, 1875, "TimerSpeedKill")
+
+local enrageTimer			= mod:NewBerserkTimer(615)
 
 local guids = {}
 local surgeTargets = {}
@@ -76,8 +78,11 @@ function mod:SPELL_AURA_APPLIED(args)
 end
 
 function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(56272) then
---		timerArcaneBreathCD:Start()
+	if args:IsSpellID(56505) then--His deep breath
+		warnBreath:Show()
+		specWarnBreath:Show()
+		timerBreath:Start()
+		timerBreathCD:Start()
 	end
 end
 
@@ -133,15 +138,12 @@ function mod:OnSync(event, arg)
 		timerSpark:Cancel()
 		timerVortexCD:Cancel()
 		warnVortexSoon:Cancel()
-		timerBreath:Start(92)
-	elseif event == "Breath" then
-		timerBreath:Schedule(1)
-		warnBreath:Schedule(1)
+		timerBreathCD:Start(94)
 	elseif event == "BreathSoon" then
 		warnBreathInc:Show()
 	elseif event == "Phase3" then
 		self:Schedule(6, buildGuidTable)
-		timerBreath:Stop()
-		timerStaticFieldCD:Start(49.5)--Consistent?
+		timerBreathCD:Cancel()
+--		timerStaticFieldCD:Start(49.5)--Consistent?
 	end
 end
--- trunk/DBM-EyeOfEternity/localization.en.lua
+++ trunk/DBM-EyeOfEternity/localization.en.lua
@@ -10,25 +10,18 @@ L:SetGeneralLocalization({
 })
 
 L:SetWarningLocalization({
-	WarningBreathSoon	= "Deep Breath soon",
-	WarningBreath		= "Deep Breath"
 })
 
 L:SetTimerLocalization({
-	TimerBreath	= "Next Deep Breath"
 })
 
 L:SetOptionLocalization({
-	WarningBreathSoon	= "Show pre-warning for Deep Breath",
-	WarningBreath		= "Show warning for Deep Breath",
-	TimerBreath			= "Show timer for next Deep Breath"
 })
 
 L:SetMiscLocalization({
 	YellPull	= "My patience has reached its limit. I will be rid of you!",
 	EmoteSpark	= "A Power Spark forms from a nearby rift!",
 	YellPhase2	= "I had hoped to end your lives quickly",
-	EmoteBreath	= "%s takes a deep breath.",
 	YellBreath	= "You will not succeed while I draw breath!",
 	YellPhase3	= "Now your benefactors make their"
 })
