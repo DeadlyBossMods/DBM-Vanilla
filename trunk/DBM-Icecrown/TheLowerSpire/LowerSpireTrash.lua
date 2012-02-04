1820e7c0715deebcf9314490d481733368f46a31
Omega <Omega@d0860a8b-7ba9-43f0-818a-d428a84863f8>
2012-02-04 03:48:22 +0000

    Kara Fixed some missed spot in ICC

--- trunk/DBM-Icecrown/TheLowerSpire/LowerSpireTrash.lua
+++ trunk/DBM-Icecrown/TheLowerSpire/LowerSpireTrash.lua
@@ -14,25 +14,18 @@ mod:RegisterEvents(
 
 local warnDisruptingShout		= mod:NewSpellAnnounce(71022, 2)
 local warnDarkReckoning			= mod:NewTargetAnnounce(69483, 3)
---local warnDeathPlague			= mod:NewTargetAnnounce(72865, 4)
 
 local specWarnDisruptingShout	= mod:NewSpecialWarningCast(71022)
 local specWarnDarkReckoning		= mod:NewSpecialWarningMove(69483)
---local specWarnDeathPlague		= mod:NewSpecialWarningYou(72865)
 local specWarnTrap				= mod:NewSpecialWarning("SpecWarnTrap")
 
 local timerDisruptingShout		= mod:NewCastTimer(3, 71022)
 local timerDarkReckoning		= mod:NewTargetTimer(8, 69483)
---local timerDeathPlague			= mod:NewTargetTimer(15, 72865)
 
 local soundDarkReckoning = mod:NewSound(69483)
 mod:AddBoolOption("SetIconOnDarkReckoning", true)
---mod:AddBoolOption("SetIconOnDeathPlague", true)
 mod:RemoveOption("HealthFrame")
 mod:RemoveOption("SpeedKillTimer")
-
---local DeathPlagueTargets = {}
---local DeathPlagueIcons = 8
 
 local function warnPlagueTargetsTargets()
 	warnDeathPlague:Show(table.concat(DeathPlagueTargets, "<, >"))
@@ -51,18 +44,6 @@ function mod:SPELL_AURA_APPLIED(args)
 		if self.Options.SetIconOnDarkReckoning then
 			self:SetIcon(args.destName, 8, 8)
 		end
---[[	elseif args:IsSpellID(72865) then
-		DeathPlagueTargets[#DeathPlagueTargets + 1] = args.destName
-		timerDeathPlague:Start(args.destName)
-		if args:IsPlayer() then
-			specWarnDeathPlague:Show()
-		end
-		if self.Options.SetIconOnDeathPlague then
-			self:SetIcon(args.destName, DeathPlagueIcons, 15)
-			DeathPlagueIcons = DeathPlagueIcons - 1
-		end
-		self:Unschedule(warnPlagueTargetsTargets)
-		self:Schedule(0.3, warnPlagueTargetsTargets)--]]
 	end
 end
 
