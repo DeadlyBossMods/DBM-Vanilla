68c8edc22e8520c7103c22ba72769fc949466f51
ebmor <ebmor@d0860a8b-7ba9-43f0-818a-d428a84863f8>
2012-12-06 06:07:51 +0000

    deDE update (Razorgore trigger)

--- DBM-BWL/localization.de.lua
+++ DBM-BWL/localization.de.lua
@@ -16,7 +16,7 @@ L:SetOptionLocalization{
 	TimerAddsSpawn	= "Zeige Zeit bis die ersten Adds erscheinen"
 }
 L:SetMiscLocalization{
-	Phase2Emote	= "Kugel der Herrschaft verliert Energie und schaltet sich ab!",
+	Phase2Emote	= "verliert Energie und schaltet sich ab!",
 	YellPull 	= "Eindringlinge sind in die Brutstätte vorgestoßen! Schlagt Alarm! Beschützt die Eier um jeden Preis!"
 }
 -------------------
