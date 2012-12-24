531030cf52aa333ba8477acc6dff1f96555c4284
Omega <Omega@d0860a8b-7ba9-43f0-818a-d428a84863f8>
2012-12-24 02:47:11 +0000

    fix Visc mod. he doesn't fire combat log events, for anything he casts. Have to localize everything except maybe summon slime. Remove useless and spammy Flame buffet warning on firemaw

--- trunk/DBM-AQ40/Viscidus.lua
+++ trunk/DBM-AQ40/Viscidus.lua
@@ -7,7 +7,6 @@ mod:SetModelID(15686)
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
-	"SPELL_AURA_APPLIED",
 	"CHAT_MSG_MONSTER_EMOTE"
 )
 
@@ -16,32 +15,65 @@ local warnShatter		= mod:NewAnnounce("WarnShatter", 2)
 
 local timerFrozen		= mod:NewBuffActiveTimer(30, 25937)
 
+local meleeHits = 0
+
 function mod:OnCombatStart(delay)
+	meleeHits = 0
 end
 
-function mod:SPELL_AURA_APPLIED(args)
-	if args:IsSpellID(26034) then
-		warnFreeze:Show(1)
-	elseif args:IsSpellID(26036) then
-		warnFreeze:Show(2)
-	elseif args:IsSpellID(25937) then
-		warnFreeze:Show(3)
-		timerFrozen:Start()
-	end
+function mod:OnCombatEnd()
+	self:UnregisterShortTermEvents()
 end
 
 function mod:CHAT_MSG_MONSTER_EMOTE(msg)
 	if msg:find(L.Phase4) then
-		self:SendSync("Phase", 2)
-	elseif msg:find(L.Phase5) then
 		self:SendSync("Phase", 1)
+	elseif msg:find(L.Phase5) then
+		self:SendSync("Phase", 2)
 	elseif msg:find(L.Phase6) then
-		self:SendSync("Phase", 0)
+		self:SendSync("Phase", 3)
+	elseif msg:find(L.Slow) then
+		self:SendSync("Slow")
+	elseif msg:find(L.Freezing) then
+		self:SendSync("Freezing")
+	elseif msg:find(L.Frozen) then
+		self:SendSync("Frozen")
+	end
+end
+
+function mod:RANGE_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, _, _, _, overkill)
+	local cid = self:GetCIDFromGUID(destGUID)
+	if cid == 15299 then
+		meleeHits = meleeHits + 1
+		print(meleeHits)
+	end
+end
+
+function mod:SWING_DAMAGE(_, _, _, _, destGUID, _, _, _, _, overkill)
+	local cid = self:GetCIDFromGUID(destGUID)
+	if cid == 15299 then
+		meleeHits = meleeHits + 1
+		print(meleeHits)
 	end
 end
 
 function mod:OnSync(msg, arg)
 	if msg == "Phase" then
 		warnShatter:Show(tonumber(arg))
+		if tonumber(arg) == 3 then
+			timerFrozen:Cancel()
+		end
+	elseif msg == "Slow" then
+		warnFreeze:Show(1)
+	elseif msg == "Freezing" then
+		warnFreeze:Show(2)
+	elseif msg == "Frozen" then
+		self:RegisterShortTermEvents(
+			"SWING_DAMAGE",
+			"RANGE_DAMAGE"
+		)
+		meleeHits = 0
+		warnFreeze:Show(3)
+		timerFrozen:Start()
 	end
 end
