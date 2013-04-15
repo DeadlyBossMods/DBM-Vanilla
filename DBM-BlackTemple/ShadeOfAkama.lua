086343670379794def155a37dbcbe26130b068cf
Adam <MysticalOS@users.noreply.github.com>
2013-04-15 21:42:03 +0000

    Remove icon on teron when debuff fades Fix kill detection on Shade of Akama and made it pull off channelers and not just shade himself. Channelers are half of the fight.

--- DBM-BlackTemple/ShadeOfAkama.lua
+++ DBM-BlackTemple/ShadeOfAkama.lua
@@ -2,12 +2,19 @@ local mod	= DBM:NewMod("Akama", "DBM-BlackTemple")
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision$"):sub(12, -3))
-mod:SetCreatureID(22841, 23215, 23216)
+mod:SetCreatureID(23421, 22841, 23215, 23216)
 mod:SetModelID(21357)
 mod:SetZone()
 
 mod:RegisterCombat("combat")
+mod:SetMinCombatTime(30)
 
-mod:RegisterEvents(
+mod:RegisterEventsInCombat(
+	"UNIT_DIED"
 )
 
+function mod:UNIT_DIED(args)
+	if self:GetCIDFromGUID(args.destGUID) == 22841 then
+		DBM:EndCombat(self)
+	end
+end
