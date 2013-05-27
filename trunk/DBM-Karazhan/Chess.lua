53a7e06161ab0aaedc5b64986c6007da3787ffea
Omega <Omega@d0860a8b-7ba9-43f0-818a-d428a84863f8>
2013-05-27 05:16:14 +0000

    chess update. works good now

--- trunk/DBM-Karazhan/Chess.lua
+++ trunk/DBM-Karazhan/Chess.lua
@@ -14,6 +14,7 @@ mod:SetWipeTime(600)
 mod:RegisterEvents(
 	"SPELL_AURA_APPLIED",
 	"SPELL_AURA_REMOVED",
+	"SPELL_CAST_SUCCESS",
 	"CHAT_MSG_RAID_BOSS_EMOTE"
 )
 
@@ -24,12 +25,15 @@ local timerNextCheat		= mod:NewTimer(108, "timerCheat", 39342)
 local firstCheat = false
 
 function mod:OnCombatStart(delay)
-	firstCheat = false
+--	firstCheat = false
+	timerNextCheat:Start(111)
 end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args.spellId == 39331 and not self:IsInCombat() then
 		DBM:StartCombat(self, 0)
+	elseif args.spellId == 30529 and args:IsPlayer() then
+		timerRecentlyInGame:Start()
 	end
 end
 
@@ -39,13 +43,22 @@ function mod:SPELL_AURA_REMOVED(args)
 	end
 end
 
+function mod:SPELL_CAST_SUCCESS(args)
+	if args.spellId == 37471 then
+		timerHeroism:Start()
+	end
+end
+
+--Cheat detection can be detected with unit event but only if medivh is focus or target. otherwise we have to use emote. Since almost no one would do this, we just use emote method
+--"<120.0 00:59:30> [UNIT_SPELLCAST_SUCCEEDED] Echo of Medivh [[focus:Karazhan - Chess, Medivh CHEAT: Fury of Medivh, Target Alliance::0:39344]]", -- [367]
+--"<120.0 00:59:30> [CHAT_MSG_RAID_BOSS_EMOTE] CHAT_MSG_RAID_BOSS_EMOTE#Echo of Medivh cheats!#Echo of Medivh###Omegall##0#0##0#3458#nil#0#false#false", -- [368]
 function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
 	if msg == L.EchoCheats then
-		if not firstCheat then
+--[[		if not firstCheat then
 			firstCheat = true
 			timerNextCheat:Start(120)
-		else
+		else--]]
 			timerNextCheat:Start()
-		end
+--		end
 	end
 end
