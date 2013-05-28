2892c6ba4ff258757b8ea22682896fb795315e19
Omega <Omega@d0860a8b-7ba9-43f0-818a-d428a84863f8>
2013-05-28 04:34:37 +0000

    This is about the best alar mod is gonna get

--- trunk/DBM-TheEye/Alar.lua
+++ trunk/DBM-TheEye/Alar.lua
@@ -26,7 +26,7 @@ local specWarnFire		= mod:NewSpecialWarningMove(35383)
 local timerQuill		= mod:NewCastTimer(10, 34229)
 local timerMeteor		= mod:NewCDTimer(54, 35181)
 local timerArmor		= mod:NewTargetTimer(60, 35410)
-local timerNextPlatform	= mod:NewTimer(34.5, "NextPlatform", 40192)--This has no spell trigger, the target scanning bosses target is still required if loop isn't accurate enough.
+local timerNextPlatform	= mod:NewTimer(34, "NextPlatform", 40192)--This has no spell trigger, the target scanning bosses target is still required if loop isn't accurate enough.
 
 local berserkTimer		= mod:NewBerserkTimer(600)
 
@@ -37,6 +37,8 @@ local flying = false
 local phase2 = false
 
 --Loop doesn't work do to varying travel time between platforms. We just need to do target scanning and start next platform timer when Al'ar reaches a platform and starts targeting player again
+--Still semi inaccurate. Sometimes Al'ar changes platforms 5-8 seconds early with no explanation. I have a feeling it's just tied to Al'ars behavior being buggy with one person.
+--I don't remember code being faulty when you actually had 4 people up there.
 local function Platform()--An attempt to avoid ugly target scanning, but i get feeling this won't be accurate enough.
 	timerNextPlatform:Start()
 	flying = false
@@ -47,6 +49,14 @@ local function Add()--An attempt to avoid ugly target scanning, but i get feelin
 	flying = true
 end
 
+function mod:OnCombatStart(delay)
+	self:AntiSpam(30, 1)--Prevent it thinking add spawn on pull and messing up first platform timer
+	flying = false
+	phase2 = false
+	warnPhase1:Show()
+	timerNextPlatform:Start(35-delay)
+end
+
 mod:RegisterOnUpdateHandler(function(self)
 	if self:IsInCombat() then
 		local foundIt
@@ -54,31 +64,28 @@ mod:RegisterOnUpdateHandler(function(self)
 		for uId in DBM:GetGroupMembers() do
 			if self:GetCIDFromGUID(UnitGUID(uId.."target")) == 19514 then
 				foundIt = true
-				target = UnitName(uId)
+				target = UnitName(uId.."targettarget")
 				if not target and UnitCastingInfo(uId.."target") == buffetName then
 					target = "Dummy"
 				end
+				--print(target, foundIt)
 				break
 			end
 		end
 		
-		if foundIt and not target and not phase2 then--Al'ar is no longer targeting anything, which means he spawned an add and is moving platforms
+		if foundIt and not target and not phase2 and self:AntiSpam(30, 1) then--Al'ar is no longer targeting anything, which means he spawned an add and is moving platforms
 			Add()
-		elseif not target and type(phase2) == "number" and (GetTime() - phase2) > 25 then--No target in phase 2 means meteor
+			--Could also be quills though, which is why we can't really put in an actual add warning.
+			--print("Add")
+		elseif not target and type(phase2) == "number" and self:AntiSpam(30, 2) and (GetTime() - phase2) > 25 then--No target in phase 2 means meteor
 			warnMeteor:Show()
 			timerMeteor:Start()
 		elseif target and flying then--Al'ar has reached a platform and is once again targeting aggro player
 			Platform()
+			--print("Platform")
 		end
 	end
-end, 0.2)
-
-function mod:OnCombatStart(delay)
-	flying = false
-	phase2 = false
-	warnPhase1:Show()
-	timerNextPlatform:Start(-delay)
-end
+end, 0.25)
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args.spellId == 34229 then
