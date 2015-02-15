fd751a66f3090c645117d79b1ab0938c583ee9a4
nbluewiz <nbluewiz@d0860a8b-7ba9-43f0-818a-d428a84863f8>
2015-02-15 13:36:14 +0000

    # fix error in Leotheras.

--- trunk/DBM-Serpentshrine/Leotheras.lua
+++ trunk/DBM-Serpentshrine/Leotheras.lua
@@ -111,7 +111,7 @@ function mod:CHAT_MSG_MONSTER_YELL(msg)
 		self:Schedule(60, humanWarns)
 	elseif msg == L.YellPhase2 or msg:find(L.YellPhase2) then
 		phase2 = true
-		self:Unschedule(60, humanWarns)
+		self:Unschedule(humanWarns)
 		timerPhase:Cancel()
 		timerWhirl:Cancel()
 		timerWhirlCD:Cancel()
