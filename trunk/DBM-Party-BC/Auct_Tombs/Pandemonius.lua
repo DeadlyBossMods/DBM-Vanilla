b1e80398aa01bc41a71c992c693181a7cb25233d
Omega <Omega@d0860a8b-7ba9-43f0-818a-d428a84863f8>
2015-08-15 23:17:10 +0000

    Fix reflect warning. it should be on by default for CASTERS not melee. It should also have target name

--- trunk/DBM-Party-BC/Auct_Tombs/Pandemonius.lua
+++ trunk/DBM-Party-BC/Auct_Tombs/Pandemonius.lua
@@ -10,13 +10,13 @@ mod:RegisterEventsInCombat(
 	"SPELL_CAST_START 32358 38759"
 )
 
-local specWarnShell			= mod:NewSpecialWarningReflect(32358, "Melee")
+local specWarnShell			= mod:NewSpecialWarningReflect(32358, "-Melee", nil, 2, 1)
 
 local timerShell			= mod:NewBuffActiveTimer(7, 32358)
 
 function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(32358, 38759) then
-		specWarnShell:Show()
+		specWarnShell:Show(args.sourceName)
 		timerShell:Start()
 	end
 end
