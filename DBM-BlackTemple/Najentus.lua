d653a5d57d6f27231e0f49b81fe312a6d9782c6c
Adam <MysticalOS@users.noreply.github.com>
2017-05-19 05:43:38 +0000

    Don’t show info frame when not time walking Better icons for human/demon form

--- DBM-BlackTemple/Najentus.lua
+++ DBM-BlackTemple/Najentus.lua
@@ -36,7 +36,7 @@ function mod:OnCombatStart(delay)
 	if self.Options.RangeFrame then
 		DBM.RangeCheck:Show(8)
 	end
-	if self.Options.InfoFrame then
+	if self.Options.InfoFrame and not self:IsTrivial(80) then
 		DBM.InfoFrame:SetHeader(L.HealthInfo)
 		DBM.InfoFrame:Show(5, "health", 8800)
 	end
