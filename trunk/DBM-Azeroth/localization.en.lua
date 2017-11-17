9a42a7008b0ec6c1f29f5d59b32492bfbb1ff532
Omega <Omega@d0860a8b-7ba9-43f0-818a-d428a84863f8>
2017-11-17 01:12:08 +0000

    Added support for 3 of the 7 classic world bosses (TW versions at least) Fixed some regressions in Karazhan and Tempest keep from last update

---
+++ trunk/DBM-Azeroth/localization.en.lua
@@ -0,0 +1,34 @@
+﻿local L
+
+-- Lord Kazzak
+L = DBM:GetModLocalization("KazzakClassic")
+
+L:SetGeneralLocalization{
+	name = "Lord Kazzak"
+}
+
+L:SetMiscLocalization({
+	Pull		= "Kazzak is supreme!"
+})
+
+-- Azuregos
+L = DBM:GetModLocalization("Azuregos")
+
+L:SetGeneralLocalization{
+	name = "Azuregos"
+}
+
+L:SetMiscLocalization({
+	Pull		= "This place is under my protection. The mysteries of the arcane shall remain inviolate."
+})
+
+-- Taerar
+L = DBM:GetModLocalization("Taerar")
+
+L:SetGeneralLocalization{
+	name = "Taerar"
+}
+
+L:SetMiscLocalization({
+	Pull		= "Peace is but a fleeting dream! Let the NIGHTMARE reign!"
+})
