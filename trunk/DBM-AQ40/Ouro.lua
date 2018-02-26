641a0d5ac207750e52396c642d7a978582f5eb97
Omega <Omega@d0860a8b-7ba9-43f0-818a-d428a84863f8>
2018-02-26 22:13:38 +0000

    Fix invalid timer field, which should correct timer color for emerge timer on Ouro

--- trunk/DBM-AQ40/Ouro.lua
+++ trunk/DBM-AQ40/Ouro.lua
@@ -23,7 +23,7 @@ local warnEnrageSoon	= mod:NewSoonAnnounce(26615, 2)
 local specWarnBlast		= mod:NewSpecialWarningSpell(26102, nil, nil, nil, 2, 2)
 
 --local timerSubmerge		= mod:NewTimer(30, "TimerSubmerge", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendBurrow.blp", nil, nil, nil, 6)
-local timerEmerge		= mod:NewTimer(30, "TimerEmerge", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendUnBurrow.blp", nil, nil, nil, 6)
+local timerEmerge		= mod:NewTimer(30, "TimerEmerge", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendUnBurrow.blp", nil, nil, 6)
 local timerSweepCD		= mod:NewNextTimer(20.5, 26103, nil, "Tank", 2, 5, nil, DBM_CORE_TANK_ICON)
 local timerBlastCD		= mod:NewNextTimer(23, 26102, nil, nil, nil, 2)
 
