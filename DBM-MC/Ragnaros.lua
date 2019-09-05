eb34d7e4019f4f9cfbec3894c7c316e21e323f13
MysticalOS <mysticalosx@gmail.com>
2019-09-05 01:04:01 -0400

    Fix same impossible lua error here as well in Addition, retail now has better submerge detection using a retail only method. No longer relies on yells. Too bad that can't be used on classic :\

--- DBM-MC/Ragnaros.lua
+++ DBM-MC/Ragnaros.lua
@@ -12,14 +12,14 @@ mod:RegisterEvents(
 )
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_SUCCESS 20566",
-	"UNIT_DIED"
+	"UNIT_DIED",
+	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
 --[[
 ability.id = 20566 and type = "cast" or target.id = 12143 and type = "death"
 --]]
 local warnWrathRag		= mod:NewSpellAnnounce(20566, 3)
-local WarnAddsLeft		= mod:NewAnnounce("WarnAddsLeft", 2, 8985)
 local warnSubmerge		= mod:NewAnnounce("WarnSubmerge", 2, "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendBurrow.blp")
 local warnEmerge		= mod:NewAnnounce("WarnEmerge", 2, "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendUnBurrow.blp")
 
@@ -29,8 +29,10 @@ local timerEmerge		= mod:NewTimer(90, "TimerEmerge", "Interface\\AddOns\\DBM-Cor
 local timerCombatStart	= mod:NewCombatTimer(73)
 
 mod.vb.addLeft = 8
+local addsGuidCheck = {}
 
 function mod:OnCombatStart(delay)
+	table.wipe(addsGuidCheck)
 	self.vb.addLeft = 8
 	timerWrathRag:Start(26.7-delay)
 	timerSubmerge:Start(180-delay)
@@ -41,6 +43,7 @@ local function emerged(self)
 	warnEmerge:Show()
 	timerWrathRag:Start(26.7)
 	timerSubmerge:Start(180)
+	table.wipe(addsGuidCheck)
 	self.vb.addLeft = 8
 end
 
@@ -51,7 +54,26 @@ function mod:SPELL_CAST_SUCCESS(args)
 	end
 end
 
+function mod:UNIT_DIED(args)
+	local guid = args.destGUID
+	local cid = self:GetCIDFromGUID(guid)
+	if cid == 12143 then--Son of Flame
+		self:SendSync("AddDied", guid)--Send sync it died do to combat log range and size of room
+		--We're in range of event, no reason to wait for sync, especially in a raid that might not have many DBM users
+		if not addsGuidCheck[guid] then
+			addsGuidCheck[guid] = true
+			self.vb.addLeft = self.vb.addLeft - 1
+			if self.vb.addLeft == 0 then--After all 8 die he emerges immediately
+				self:Unschedule(emerged)
+				emerged(self)
+			end
+		end
+	end
+end
+
 function mod:CHAT_MSG_MONSTER_YELL(msg)
+	--"<264.91 00:32:46> [UNIT_SPELLCAST_SUCCEEDED] Ragnaros(??) -Ragnaros Submerge Visual- [[boss1:Cast-3-3882-409-7508-20567-0000F08FEE:20567]]", -- [475]
+	--"<265.01 00:32:46> [CHAT_MSG_MONSTER_YELL] COME FORTH, MY SERVANTS! DEFEND YOUR MASTER!#Ragnaros#####0#0##0#91#nil#0#false#false#false#false", -- [481]
 	if msg == L.Submerge then
 		self:SendSync("Submerge")
 	--Could also use this instead
@@ -59,6 +81,17 @@ function mod:CHAT_MSG_MONSTER_YELL(msg)
 	--"<38.0 22:36:31> [CHAT_MSG_MONSTER_YELL] CHAT_MSG_MONSTER_YELL#Impudent whelps! You've rushed headlong to your own deaths! See now, the master stirs!\r\n#Majordomo Executus###Shiramura
 	elseif msg == L.Pull then
 		timerCombatStart:Start()
+	end
+end
+
+--Retail only, no UNIT events in classic
+function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
+	if spellId == 20567 then--Ragnaros Submerge Visual
+		self:Unschedule(emerged)
+		timerWrathRag:Cancel()
+		warnSubmerge:Show()
+		timerEmerge:Start(90)
+		self:Schedule(90, emerged, self)
 	end
 end
 
@@ -70,18 +103,13 @@ function mod:OnSync(msg)
 		warnSubmerge:Show()
 		timerEmerge:Start(90)
 		self:Schedule(90, emerged, self)
-	end
-end
-
-function mod:UNIT_DIED(args)
-	local cid = self:GetCIDFromGUID(args.destGUID)
-	if cid == 12143 then--Son of Flame
+	elseif msg == "AddDied" and guid and not addsGuidCheck[guid] then
+		--A unit died we didn't detect ourselves, so we correct our adds counter from sync
+		addsGuidCheck[guid] = true
 		self.vb.addLeft = self.vb.addLeft - 1
 		if self.vb.addLeft == 0 then--After all 8 die he emerges immediately
 			self:Unschedule(emerged)
 			emerged(self)
-		elseif self.vb.addLeft < 4 then
-			WarnAddsLeft(self.vb.addLeft)
 		end
 	end
 end
