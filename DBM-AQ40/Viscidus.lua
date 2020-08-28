1ef9bdd3a1351768bb221319409a1e925d2bf550
MysticalOS <mysticalosx@gmail.com>
2020-08-28 13:58:31 -0400

    Sync a few classic changes over to retail AQ40 and AQ20

--- DBM-AQ40/Viscidus.lua
+++ DBM-AQ40/Viscidus.lua
@@ -5,48 +5,85 @@ mod:SetRevision("@file-date-integer@")
 mod:SetCreatureID(15299)
 mod:SetEncounterID(713)
 mod:SetModelID(15686)
-mod:SetMinSyncRevision(428)
+mod:SetHotfixNoticeRev(20200828000000)--2020, 8, 28
+mod:SetMinSyncRevision(20200828000000)--2020, 8, 28
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
+	"SPELL_CAST_SUCCESS 25991",
+	"SPELL_AURA_APPLIED 25989",
 	"CHAT_MSG_MONSTER_EMOTE"
 )
 
-local warnFreeze		= mod:NewAnnounce("WarnFreeze", 2, 33395)
-local warnShatter		= mod:NewAnnounce("WarnShatter", 2, 12982)
+local warnPoisonBoltVolley		= mod:NewCountAnnounce(25991, 3)
+local warnFreeze				= mod:NewAnnounce("WarnFreeze", 2, 33395)
+local warnShatter				= mod:NewAnnounce("WarnShatter", 2, 12982)
 
-local timerFrozen		= mod:NewBuffActiveTimer(30, 25937, nil, nil, nil, 6)
+local specWarnGTFO				= mod:NewSpecialWarningGTFO(25989, nil, nil, nil, 1, 8)
 
-function mod:CHAT_MSG_MONSTER_EMOTE(msg)
-	if msg:find(L.Phase4) then
-		self:SendSync("Phase", 1)
-	elseif msg:find(L.Phase5) then
-		self:SendSync("Phase", 2)
-	elseif msg:find(L.Phase6) then
-		self:SendSync("Phase", 3)
-	elseif msg:find(L.Slow) then
-		self:SendSync("Slow")
-	elseif msg:find(L.Freezing) then
-		self:SendSync("Freezing")
-	elseif msg:find(L.Frozen) then
-		self:SendSync("Frozen")
+local timerPoisonBoltVolleyCD	= mod:NewCDCountTimer(11, 25991, nil, nil, nil, 2, nil, DBM_CORE_L.POISON_ICON)
+local timerFrozen				= mod:NewBuffActiveTimer(30, 25937, nil, nil, nil, 6)
+
+mod.vb.Frozen = false
+mod.vb.volleyCount = 0
+
+function mod:OnCombatStart(delay)
+	self.vb.Frozen = false
+	self.vb.volleyCount = 0
+	timerPoisonBoltVolleyCD:Start(12.9, 1)
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args.spellId == 25991 then
+		self.vb.volleyCount = self.vb.volleyCount + 1
+		warnPoisonBoltVolley:Show(self.vb.volleyCount)
+		timerPoisonBoltVolleyCD:Start(11, self.vb.volleyCount+1)
+		if self.vb.Frozen then
+			--Boss casts this near instantly coming out of frozen
+			self.vb.Frozen = false
+			timerFrozen:Stop()
+			warnShatter:Show(3)
+		end
 	end
 end
 
-function mod:OnSync(msg, arg)
-	if msg == "Phase" then
-		local count = tonumber(arg)
+function mod:SPELL_AURA_APPLIED(args)
+	if args.spellId == 25989 and args:IsPlayer() and self:AntiSpam(3, 2) and not self:IsTrivial(80) then
+		specWarnGTFO:Show(args.spellName)
+		specWarnGTFO:Play("watchfeet")
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_EMOTE(msg)
+	if msg == L.Phase4 or msg:find(L.Phase4) then
+		self:SendSync("Shatter", 1)
+	elseif msg == L.Phase5 or msg:find(L.Phase5) then
+		self:SendSync("Shatter", 2)
+	elseif msg == L.Phase6 or msg:find(L.Phase6) then--No longer present in classic?
+		self:SendSync("Shatter", 3)
+	elseif msg == L.Slow or msg:find(L.Slow) then
+		self:SendSync("Freeze", 1)
+	elseif msg == L.Freezing or msg:find(L.Freezing) then
+		self:SendSync("Freeze", 2)
+	elseif msg == L.Frozen or msg:find(L.Frozen) then
+		self:SendSync("Freeze", 3)
+	end
+end
+
+function mod:OnSync(msg, count)
+	if msg == "Shatter" and count then
+		count = tonumber(count)
 		warnShatter:Show(count)
+--		if count == 3 then
+--			timerFrozen:Stop()
+--		end
+	elseif msg == "Freeze" and count then
+		count = tonumber(count)
+		warnFreeze:Show(count)
 		if count == 3 then
-			timerFrozen:Stop()
+			timerFrozen:Start()
+			timerPoisonBoltVolleyCD:Stop()
 		end
-	elseif msg == "Slow" then
-		warnFreeze:Show(1)
-	elseif msg == "Freezing" then
-		warnFreeze:Show(2)
-	elseif msg == "Frozen" then
-		warnFreeze:Show(3)
-		timerFrozen:Start()
 	end
 end
--- DBM-MC/MCTrash.lua
+++ DBM-MC/MCTrash.lua
@@ -36,7 +36,7 @@ do
 				self.vb.firstEngageTime = GetServerTime()
 				if self.Options.FastestClear2 and self.Options.SpeedClearTimer then
 					--Custom bar creation that's bound to core, not mod, so timer doesn't stop when mod stops it's own timers
-					DBM.Bars:CreateBar(self.Options.FastestClear2, DBM_CORE_L.SPEED_CLEAR_TIMER_TEXT, "136106")
+					DBM.Bars:CreateBar(self.Options.FastestClear2, DBM_CORE_L.SPEED_CLEAR_TIMER_TEXT, 136106)
 				end
 				self:SendSync("MCStarted", self.vb.firstEngageTime)--Also sync engage time
 			end
