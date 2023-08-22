eefa0b75ada0a6d874f044a5c217d295d2659918
MysticalOS <mysticalosx@gmail.com>
2023-08-21 23:58:19 -0400

    classic toc update

--- DBM-Raids-Vanilla/VanillaNaxx/ConstructQuarter/Thaddius.lua
+++ DBM-Raids-Vanilla/VanillaNaxx/ConstructQuarter/Thaddius.lua
@@ -29,9 +29,6 @@ local timerNextShift		= mod:NewCDTimer(25.9, 28089, nil, nil, nil, 2, nil, DBM_C
 local timerShiftCast		= mod:NewCastTimer(3, 28089, nil, nil, nil, 5)
 local timerThrow			= mod:NewCDTimer(20.6, 28338, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)
 
-if not DBM.Options.GroupOptionsBySpell then
-	mod:AddMiscLine(DBM_CORE_L.OPTION_CATEGORY_DROPDOWNS)
-end
 mod:AddDropdownOption("AirowsEnabled", {"Never", "TwoCamp", "ArrowsRightLeft", "ArrowsInverse"}, "Never", "misc", nil, 28089)
 
 local currentCharge
