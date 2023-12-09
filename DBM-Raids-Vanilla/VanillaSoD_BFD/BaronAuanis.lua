da2ddbc114d5cf4192a78619077bee05696e1b9f
MysticalOS <mysticalosx@gmail.com>
2023-12-08 23:04:36 -0500

    Massively improve Baron and Ghamoora mods Improved Kelris mod with a personal dream timer and better P2 detection and shadowcrawsh announces

--- DBM-Raids-Vanilla/VanillaSoD_BFD/BaronAuanis.lua
+++ DBM-Raids-Vanilla/VanillaSoD_BFD/BaronAuanis.lua
@@ -4,36 +4,36 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision("@file-date-integer@")
 mod:SetCreatureID(202699)
 mod:SetEncounterID(2694)--2765 is likely 5 man version in instance type 201 (which has Old Serra'kis instead of Baron)
-mod:SetHotfixNoticeRev(20231201000000)
+mod:SetHotfixNoticeRev(20231208000000)
 --mod:SetMinSyncRevision(20231115000000)
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
---	"SPELL_CAST_START",
+	"SPELL_CAST_SUCCESS 404806",
 	"SPELL_AURA_APPLIED 404806",
 	"SPELL_AURA_REMOVED 404806"
 )
 
+--NOTE: depth charge might reset to 14.4 on boss gaining bubble beam
+--ability.id = 404806 and type = "cast" or ability.id = 413664 and (type = "applybuff" or type = "removebuff")
 local warningDepthCharge		= mod:NewTargetNoFilterAnnounce(404806, 4)
 
 local specWarnDepthCharge		= mod:NewSpecialWarningMoveTo(404806, nil, nil, nil, 3, 2)
 local yellDepthCharge			= mod:NewYell(404806)
 local yellDepthChargeFades		= mod:NewShortFadesYell(404806)
 
---local timerSleepCD			= mod:NewAITimer(180, 8399, nil, nil, nil, 3, nil, DBM_COMMON_L.MAGIC_ICON)
+local timerDepthChargeCD		= mod:NewCDTimer(16.1, 404806, nil, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON)--Can be massively dealyed by Bubble Beam
 
---function mod:OnCombatStart(delay)
+function mod:OnCombatStart(delay)
+	timerDepthChargeCD:Start(17.8-delay)
+end
 
---end
-
---[[
-function mod:SPELL_CAST_START(args)
-	if args:IsSpell(8399) and args:IsSrcTypeHostile() then
-
+function mod:SPELL_CAST_SUCCESS(args)
+	if args:IsSpell(404806) then
+		timerDepthChargeCD:Start()
 	end
 end
---]]
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpell(404806) and args:IsDestTypePlayer() then
