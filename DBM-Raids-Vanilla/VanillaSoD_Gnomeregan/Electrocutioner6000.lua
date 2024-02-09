b81c77d3bb9aeaf78eafd1eb20e5d0bea84fc77e
MysticalOS <mysticalosx@gmail.com>
2024-02-08 20:32:40 -0500

    Preliminary Electrocutioner 6000 mod

--- DBM-Raids-Vanilla/VanillaSoD_Gnomeregan/Electrocutioner6000.lua
+++ DBM-Raids-Vanilla/VanillaSoD_Gnomeregan/Electrocutioner6000.lua
@@ -9,52 +9,59 @@ mod:SetEncounterID(2927)
 
 mod:RegisterCombat("combat")
 
---mod:RegisterEventsInCombat(
---	"SPELL_CAST_START",
---	"SPELL_CAST_SUCCESS",
---	"SPELL_AURA_APPLIED",
+mod:RegisterEventsInCombat(
+	"SPELL_CAST_START 433251",
+	"SPELL_CAST_SUCCESS 433359",
+	"SPELL_AURA_APPLIED 433359"
 --	"SPELL_AURA_APPLIED_DOSE",
 --	"SPELL_AURA_REMOVED"
---)
+)
 
+--TODO, https://www.wowhead.com/classic/spell=433398/discombobulation-protocol is an instant cast so can't really prewarn it?
 --local warnCorrosion				= mod:NewStackAnnounce(427625, 2, nil, "Tank|Healer")
 --local warnDarkProtection		= mod:NewSpellAnnounce(429541, 3)
 
---local specWarnCorrosiveBlast	= mod:NewSpecialWarningDodge(429168, nil, nil, nil, 2, 2)
---local yellDepthCharge			= mod:NewYell(404806)
+local specWarnStaticArc				= mod:NewSpecialWarningCount(433251, nil, nil, nil, 2, 2)
+local specWarnMagneticPulse			= mod:NewSpecialWarningMoveAway(433359, nil, nil, nil, 1, 2)
+local yellMagneticPulse				= mod:NewYell(433359)
 
---local timerCorrosiveBlastCD		= mod:NewCDTimer(21, 429168, nil, nil, nil, 3)
+local timerStaticArcCD				= mod:NewAITimer(21, 433251, nil, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON)
+local timerMagneticPulseCD			= mod:NewAITimer(21, 433359, nil, nil, nil, 3)
 --local timerCorrosiveBiteCD		= mod:NewCDTimer(6.5, 429207, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON)
 
+mod.vb.arcCount = 0
 
---function mod:OnCombatStart(delay)
+function mod:OnCombatStart(delay)
+	self.vb.arcCount = 0
+	timerMagneticPulseCD:Start(1)
+	timerStaticArcCD:Start(1)
+end
 
---end
-
---[[
 function mod:SPELL_CAST_START(args)
-	if args:IsSpell(429168) then
-
+	if args:IsSpell(433251) then
+		self.vb.arcCount = self.vb.arcCount + 1
+		specWarnStaticArc:Show(self.vb.arcCount)
+		specWarnStaticArc:Play("specialsoon")
+		timerStaticArcCD:Start()
 	end
 end
---]]
 
---[[
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpell(429207) then
-
+	if args:IsSpell(429207) and self:AntiSpam(3, 1) then
+		timerMagneticPulseCD:Start()
 	end
 end
---]]
 
---[[
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpell(427625) then
-
+		if args:IsPlayer() then
+			specWarnMagneticPulse:Show()
+			specWarnMagneticPulse:Play("runout")
+			yellMagneticPulse:Yell()
+		end
 	end
 end
 --mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
---]]
 
 --[[
 function mod:SPELL_AURA_REMOVED(args)
