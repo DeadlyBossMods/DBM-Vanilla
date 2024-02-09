1909c67c1d26d66aa9da9d884f02d8056870931d
MysticalOS <mysticalosx@gmail.com>
2024-02-08 19:50:03 -0500

    Add preliminary Viscous Fallout features

--- DBM-Raids-Vanilla/VanillaSoD_Gnomeregan/ViscousFallout.lua
+++ DBM-Raids-Vanilla/VanillaSoD_Gnomeregan/ViscousFallout.lua
@@ -2,42 +2,50 @@ local mod	= DBM:NewMod("ViscousFalloutSoD", "DBM-Raids-Vanilla", 8)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("@file-date-integer@")
-mod:SetCreatureID(220007)
+mod:SetCreatureID(220007)--217308 Irradiated Goo adds
 mod:SetEncounterID(2928)
+mod:SetUsedIcons(8, 7, 6)
 --mod:SetHotfixNoticeRev(20231201000000)
 --mod:SetMinSyncRevision(20231115000000)
 
 mod:RegisterCombat("combat")
 
 --mod:RegisterEventsInCombat(
---	"SPELL_CAST_START",
+	"SPELL_CAST_START 434358",
 --	"SPELL_CAST_SUCCESS",
+	"SPELL_SUMMON 434762",
 --	"SPELL_AURA_APPLIED",
 --	"SPELL_AURA_APPLIED_DOSE",
---	"SPELL_AURA_REMOVED"
+--	"SPELL_AURA_REMOVED",
+	"SPELL_PERIODIC_DAMAGE 434433",
+	"SPELL_PERIODIC_MISSED 434433"
 --)
 
 --local warnCorrosion				= mod:NewStackAnnounce(427625, 2, nil, "Tank|Healer")
---local warnDarkProtection		= mod:NewSpellAnnounce(429541, 3)
+local warnSummonIrradiatedGoo		= mod:NewSpellAnnounce(434358, 3)
 
---local specWarnCorrosiveBlast	= mod:NewSpecialWarningDodge(429168, nil, nil, nil, 2, 2)
---local yellDepthCharge			= mod:NewYell(404806)
+--local specWarnCorrosiveBlast		= mod:NewSpecialWarningDodge(429168, nil, nil, nil, 2, 2)
+--local yellDepthCharge				= mod:NewYell(404806)
+local specWarnGTFO					= mod:NewSpecialWarningGTFO(434433, nil, nil, nil, 1, 8)
 
---local timerCorrosiveBlastCD		= mod:NewCDTimer(21, 429168, nil, nil, nil, 3)
+local timerSummonIrradiatedGooCD	= mod:NewAITimer(21, 434358, nil, nil, nil, 1)
 --local timerCorrosiveBiteCD		= mod:NewCDTimer(6.5, 429207, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON)
 
+mod:AddSetIconOption("SetIconOnGoo", 434358, true, 5, {8, 7, 6})
 
---function mod:OnCombatStart(delay)
+mod.vb.gooIcon = 8
 
---end
+function mod:OnCombatStart(delay)
+	timerSummonIrradiatedGooCD:Start(1)
+end
 
---[[
 function mod:SPELL_CAST_START(args)
 	if args:IsSpell(429168) then
-
+		self.vb.gooIcon = 8
+		warnSummonIrradiatedGoo:Show()
+		timerSummonIrradiatedGooCD:Start()
 	end
 end
---]]
 
 --[[
 function mod:SPELL_CAST_SUCCESS(args)
@@ -46,6 +54,14 @@ function mod:SPELL_CAST_SUCCESS(args)
 	end
 end
 --]]
+
+function mod:SPELL_SUMMON(args)
+	if args:IsSpell(434762) then
+		if self.Options.SetIconOnGoo then
+			self:ScanForMobs(args.destGUID, 2, self.vb.gooIcon, 1, nil, 12, "SetIconOnGoo")
+		end
+	end
+end
 
 --[[
 function mod:SPELL_AURA_APPLIED(args)
@@ -63,6 +79,14 @@ function mod:SPELL_AURA_REMOVED(args)
 	end
 end
 --]]
+
+function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spellName)
+	if spellId == 434433 and destGUID == UnitGUID("player") and not playerSong and self:AntiSpam(3, 1) then
+		specWarnGTFO:Show(spellName)
+		specWarnGTFO:Play("watchfeet")
+	end
+end
+mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
 
 --[[
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
