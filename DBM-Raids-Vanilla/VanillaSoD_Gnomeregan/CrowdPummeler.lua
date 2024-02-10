d6b893765aff436a10bceae2824da05cafd10ea4
MysticalOS <mysticalosx@gmail.com>
2024-02-10 01:32:35 -0500

    Crowd pummeler update

--- DBM-Raids-Vanilla/VanillaSoD_Gnomeregan/CrowdPummeler.lua
+++ DBM-Raids-Vanilla/VanillaSoD_Gnomeregan/CrowdPummeler.lua
@@ -4,65 +4,70 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision("@file-date-integer@")
 mod:SetCreatureID(215728)
 mod:SetEncounterID(2899)
---mod:SetHotfixNoticeRev(20231201000000)
+mod:SetUsedIcons(8)
+mod:SetHotfixNoticeRev(20240209000000)
 --mod:SetMinSyncRevision(20231115000000)
 
 mod:RegisterCombat("combat")
 
---mod:RegisterEventsInCombat(
---	"SPELL_CAST_START",
---	"SPELL_CAST_SUCCESS",
---	"SPELL_AURA_APPLIED",
---	"SPELL_AURA_APPLIED_DOSE",
---	"SPELL_AURA_REMOVED"
---)
-
---local warnCorrosion				= mod:NewStackAnnounce(427625, 2, nil, "Tank|Healer")
---local warnDarkProtection		= mod:NewSpellAnnounce(429541, 3)
-
---local specWarnCorrosiveBlast	= mod:NewSpecialWarningDodge(429168, nil, nil, nil, 2, 2)
---local yellDepthCharge			= mod:NewYell(404806)
-
---local timerCorrosiveBlastCD		= mod:NewCDTimer(21, 429168, nil, nil, nil, 3)
---local timerCorrosiveBiteCD		= mod:NewCDTimer(6.5, 429207, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON)
-
-
---function mod:OnCombatStart(delay)
-
---end
+mod:RegisterEventsInCombat(
+	"SPELL_CAST_START 432062",
+	"SPELL_CAST_SUCCESS 432423"
+)
 
 --[[
+ability.id = 432062 and type = "begincast"
+ or (ability.id = 436532 or ability.id = 432423) and type = "cast"
+--]]
+--TODO, announce he's murdering gnomes in bleachers? does this affect players?
+--TODO, detect blades spawning on ground? probably needs transcriptor, didn't see a CLEU event
+--TODO, true stage 2 trigger for starting claw timer and updating smash timer. Again, needs transcriptor probably a yell or emote or USCS
+local warnTheClaw					= mod:NewTargetNoFilterAnnounce(432423, 3)
+
+local specWarnGnomereganSmash		= mod:NewSpecialWarningDodge(432423, nil, nil, nil, 3, 2)
+local specWarnTheClaw				= mod:NewSpecialWarningYou(432062, nil, nil, nil, 1, 2)
+local yellTheClaw					= mod:NewYell(432062)
+
+local timerGnomereganSmashCD		= mod:NewCDTimer(11.3, 432423, nil, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON)
+local timerTheClawCD				= mod:NewCDTimer(15.2, 432062, nil, nil, nil, 3)--15-20 (needs larger sample)
+
+mod:AddSetIconOption("SetIconOnClaw", 432062, true, 0, {8})
+
+function mod:ClawTarget(targetname, uId)
+	if not targetname then return end
+	if targetname == UnitName("player") then
+		specWarnTheClaw:Show()
+		specWarnTheClaw:Play("runout")
+		yellTheClaw:Yell()
+	else
+		warnTheClaw:Show(targetname)
+	end
+	if self.Options.SetIconOnClaw then
+		self:SetIcon(targetname, 8, 3)
+	end
+end
+
+function mod:OnCombatStart(delay)
+	timerGnomereganSmashCD:Start(6.2)
+	--Claw has no initial timer, boss gains ability when his health gets low
+end
+
 function mod:SPELL_CAST_START(args)
-	if args:IsSpell(429168) then
-
+	if args:IsSpell(432062) then
+		timerTheClawCD:Start()
+		self:ScheduleMethod(0.1, "BossTargetScanner", args.sourceGUID, "ClawTarget", 0.1, 3)
 	end
 end
---]]
 
---[[
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpell(429207) then
+	if args:IsSpell(432423) then
+		specWarnGnomereganSmash:Show()
+		specWarnGnomereganSmash:Play("shockwave")
+		timerGnomereganSmashCD:Start()
+--	elseif args:IsSpell(436532) then--Throw and Despawn Closest Bleacher and Leper Gnomes
 
 	end
 end
---]]
-
---[[
-function mod:SPELL_AURA_APPLIED(args)
-	if args:IsSpell(427625) then
-
-	end
-end
---mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
---]]
-
---[[
-function mod:SPELL_AURA_REMOVED(args)
-	if args:IsSpell(429541) then
-
-	end
-end
---]]
 
 --[[
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
