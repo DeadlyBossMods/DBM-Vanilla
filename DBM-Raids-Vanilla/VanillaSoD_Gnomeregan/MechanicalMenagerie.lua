7a89f8ebaf845f0a5a068c1feaf54b828459f574
MysticalOS <mysticalosx@gmail.com>
2024-02-10 03:25:55 -0500

    And push the Menagerie mod

--- DBM-Raids-Vanilla/VanillaSoD_Gnomeregan/MechanicalMenagerie.lua
+++ DBM-Raids-Vanilla/VanillaSoD_Gnomeregan/MechanicalMenagerie.lua
@@ -2,77 +2,165 @@ local mod	= DBM:NewMod("MechanicalMenagerieSoD", "DBM-Raids-Vanilla", 8)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("@file-date-integer@")
---mod:SetCreatureID()--(no main boss ID?)
+mod:SetCreatureID(218245, 218244, 218243, 218242)--https://www.wowhead.com/classic/npc=218344/explosive-egg
 mod:SetEncounterID(2935)
 mod:SetBossHPInfoToHighest()
---mod:SetHotfixNoticeRev(20231201000000)
+mod:SetHotfixNoticeRev(20240209000000)
 --mod:SetMinSyncRevision(20231115000000)
 
 --[[
-STX-37/CN 218245 (Chicken add?)
-STX-25/NB 218244 (Squirrel Add?)
-STX-13/LL 218243 (sheep add?)
-STX-04/BD 218242 (whelp add?)
+STX-37/CN 218245 (Chicken)
+STX-25/NB 218244 (Squirrel)
+STX-13/LL 218243 (sheep)
+STX-04/BD 218242 (whelp)
 --]]
 
 mod:RegisterCombat("combat")
 
---mod:RegisterEventsInCombat(
---	"SPELL_CAST_START",
---	"SPELL_CAST_SUCCESS",
---	"SPELL_AURA_APPLIED",
+mod:RegisterEventsInCombat(
+	"SPELL_CAST_START 436692 440073 436833 436836 436824 436816 436695",
+	"SPELL_CAST_SUCCESS 436570",
+	"SPELL_SUMMON 436692",
+	"SPELL_AURA_APPLIED 436837 436828 436825 436741",
 --	"SPELL_AURA_APPLIED_DOSE",
---	"SPELL_AURA_REMOVED"
---)
+	"SPELL_AURA_REMOVED 436825"
+)
 
---NOTE: Looks like fight is just with the adds and nothing else?
---I suspect just active Ids should be in mod and 4 inactives ignored
+--NOTE: All 4 must die within 20 seconds due to Self Repair
+--NOTE: Cluck is 50% attack speed to all, maybe normalize text for it?
+--NOTE: Overload is damage bonus to targets, also maybe normalize text?
+--TODO: add https://www.wowhead.com/classic/spell=436691/peck ? seems like just standard tank damage attack
+--TODO: 2 person or 3 person interrupt rotation, if count is added to it. Or maybe a dropdown to choose which?
+--[[
+(ability.id = 436692 or ability.id = 440073 or ability.id = 436833 or ability.id = 436836 or ability.id = 436824 or ability.id = 436816 or ability.id = 436695) and type = "begincast"
+ or (ability.id = 436828 or ability.id = 436570) and type = "cast"
+--]]
+--General
+local warnSelfRepair				= mod:NewCastAnnounce(440073, 4)
+
+local timerSelfRepair				= mod:NewCastSourceTimer(20, 440073, nil, nil, nil, 5, nil, DBM_COMMON_L.DAMAGE_ICON)
+--STX-37/CN
 --local warnCorrosion				= mod:NewStackAnnounce(427625, 2, nil, "Tank|Healer")
---local warnDarkProtection		= mod:NewSpellAnnounce(429541, 3)
+local warnCluck						= mod:NewSpellAnnounce(436570, 3)
 
---local specWarnCorrosiveBlast	= mod:NewSpecialWarningDodge(429168, nil, nil, nil, 2, 2)
---local yellDepthCharge			= mod:NewYell(404806)
+local specWarnExplosiveEgg			= mod:NewSpecialWarningSwitch(436692, "Ranged", nil, nil, 1, 2)
 
---local timerCorrosiveBlastCD		= mod:NewCDTimer(21, 429168, nil, nil, nil, 3)
---local timerCorrosiveBiteCD		= mod:NewCDTimer(6.5, 429207, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON)
+local timerExplosiveEggCD			= mod:NewCDTimer(21, 436692, nil, nil, nil, 1, nil, DBM_COMMON_L.DAMAGE_ICON)--21-39 (pretty gross, but consistent min window)
 
+mod:AddSetIconOption("SetIconOnEgg", 436692, true, 5, {8})
+--STX-25/NB
+local warnWidgetFortress			= mod:NewCastAnnounce(436836, 4)
 
---function mod:OnCombatStart(delay)
+local specWarnWidgetVolley			= mod:NewSpecialWarningInterrupt(436692, "HasInterrupt", nil, nil, 1, 2)
+local specWarnWidgetFortress		= mod:NewSpecialWarningMove(436836, "Tank", nil, nil, 1, 2)
+--local yellDepthCharge				= mod:NewYell(404806)
 
---end
+--local timerWidgetFortressCD		= mod:NewCDTimer(6.5, 436836, nil, "Tank", nil, 5, nil, DBM_COMMON_L.TANK_ICON)--50-89 so no timer for now
+-- STX-13/LL
+local warnBinaryBleat				= mod:NewTargetNoFilterAnnounce(436828, 3, nil, false)--off by default optioanl announce for anyone who gets silenced for 10 seconds. ALso, no timer since 11-21 variation
+local warnFrayedOver				= mod:NewFadesAnnounce(436825, 1)
 
---[[
+local specWarnFrayedWiring			= mod:NewSpecialWarningReflect(436825, nil, 27564, nil, 1, 2)--Short name "Reflection"
+
+mod:AddNamePlateOption("NPAuraOnFrayed", 436825)
+--STX-04/BD
+local warnOverheat						= mod:NewCastAnnounce(436741, 1)
+
+local specWarnSprocketfireBreath		= mod:NewSpecialWarningSpell(436816, nil, 18351, nil, 2, 2)--Short name "Breath"
+
+local timerOverheat						= mod:NewBuffActiveTimer(15, 436741, nil, nil, nil, 5, nil, DBM_COMMON_L.DAMAGE_ICON)
+local timerSprocketfireBreathCD			= mod:NewCDTimer(21, 436816, 18351, nil, nil, 3)--21-26, first timer that's not too radically variable and we can include
+
+function mod:OnCombatStart(delay)
+--	timerWidgetFortressCD:Start(1)
+	timerSprocketfireBreathCD:Start(13)--13-23 (gross)
+--	timerExplosiveEggCD:Start(3.2)--3.2-31, yeah, nope
+	if self.Options.NPAuraOnFrayed then
+		DBM:FireEvent("BossMod_EnableHostileNameplates")
+	end
+end
+
+function mod:OnCombatEnd()
+	if self.Options.NPAuraOnFrayed then
+		DBM.Nameplate:Hide(true, nil, nil, nil, true, true)
+	end
+end
+
 function mod:SPELL_CAST_START(args)
-	if args:IsSpell(429168) then
-
+	if args:IsSpell(436692) then
+		specWarnExplosiveEgg:Show()
+		specWarnExplosiveEgg:Play("targetchange")
+		timerExplosiveEggCD:Start()
+	elseif args:IsSpell(440073) then
+		timerSelfRepair:Start(20, args.sourceName)
+		if self:AntiSpam(3, 1) then
+			warnSelfRepair:Show()
+		end
+		local cid = self:GetCIDFromGUID(args.destGUID)
+		if cid == 218242 then--STX-04/BD
+			--20 second channel, safe to assume any existing breath timer is delayed
+			timerSprocketfireBreathCD:Stop()
+		end
+	elseif args:IsSpell(436833) then
+--		timerPetrifyCD:Start()
+		if self:CheckInterruptFilter(args.sourceGUID, false, true, true) then--Since it's council boss, target/focus filter is disabled for now.
+			specWarnWidgetVolley:Show(args.sourceName)
+			specWarnWidgetVolley:Play("kickcast")
+		end
+	elseif args:IsSpell(436836) then
+		warnWidgetFortress:Show()
+--		timerWidgetFortressCD:Start()
+	elseif args:IsSpell(436824) then
+		specWarnFrayedWiring:Show(args.sourceName)
+		specWarnFrayedWiring:Play("stopattack")
+	elseif args:IsSpell(436816) then
+		specWarnSprocketfireBreath:Show()
+		specWarnSprocketfireBreath:Play("breathsoon")
+		timerSprocketfireBreathCD:Start()
+	elseif args:IsSpell(436695) then
+		warnOverheat:Show()
 	end
 end
---]]
 
---[[
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpell(429207) then
-
+	if args:IsSpell(436570) then
+		warnCluck:Show()
 	end
 end
---]]
 
---[[
+function mod:SPELL_SUMMON(args)
+	local spellId = args.spellId
+	if spellId == 436692 then
+		if self.Options.SetIconOnEgg then
+			self:ScanForMobs(args.destGUID, 2, 8, 1, nil, 12, "SetIconOnEgg")
+		end
+	end
+end
+
 function mod:SPELL_AURA_APPLIED(args)
-	if args:IsSpell(427625) then
-
+	if args:IsSpell(436837) and self:AntiSpam(3, 2) then
+		specWarnWidgetFortress:Show()
+		specWarnWidgetFortress:Play("moveboss")
+	elseif args:IsSpell(436828) then
+		warnBinaryBleat:CombinedShow(0.3, args.destName)
+	elseif args:IsSpell(436825) then
+		if self.Options.NPAuraOnFrayed then
+			DBM.Nameplate:Show(true, args.destGUID, 436825, nil, 15)
+		end
+	elseif args:IsSpell(436741) and self:AntiSpam(3, 3) then
+		timerOverheat:Start()
 	end
 end
 --mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
---]]
 
---[[
 function mod:SPELL_AURA_REMOVED(args)
-	if args:IsSpell(429541) then
-
+	if args:IsSpell(436825) then
+		warnFrayedOver:Show()
+		if self.Options.NPAuraOnFrayed then
+			DBM.Nameplate:Hide(true, args.destGUID, 436825)
+		end
 	end
 end
---]]
 
 --[[
 function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
