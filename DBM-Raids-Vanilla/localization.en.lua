379a19ffca6e891d7e9d0588044fce7efb9a059a
MysticalOS <mysticalosx@gmail.com>
2024-02-13 20:09:25 -0500

    Normalize boss names for Menagerie

--- DBM-Raids-Vanilla/localization.en.lua
+++ DBM-Raids-Vanilla/localization.en.lua
@@ -1234,6 +1234,13 @@ L:SetGeneralLocalization({
 	name = "Mechanical Menagerie"
 })
 
+L:SetMiscLocalization{
+	Sheep		= "Sheep",
+	Whelp		= "Whelp",
+	Squirrel	= "Squirrel",
+	Chicken		= "Chicken"
+}
+
 -----------------------------
 --  Mekgineer Thermaplugg  --
 -----------------------------
