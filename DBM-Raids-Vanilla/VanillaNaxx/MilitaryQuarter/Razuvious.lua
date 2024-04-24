9952b92c05c64423b2a02fd380bd81a74dfc3950
MysticalOS <mysticalosx@gmail.com>
2024-04-24 15:46:34 -0400

    Somehow missed an entire folder with the renames. Fixes and closes https://github.com/DeadlyBossMods/DBM-BCVanilla/issues/137

--- DBM-Raids-Vanilla/VanillaNaxx/MilitaryQuarter/Razuvious.lua
+++ DBM-Raids-Vanilla/VanillaNaxx/MilitaryQuarter/Razuvious.lua
@@ -1,4 +1,4 @@
-local mod	= DBM:NewMod("Razuvious", "DBM-Raids-Vanilla", 1)
+local mod	= DBM:NewMod("RazuviousVanilla", "DBM-Raids-Vanilla", 1)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("@file-date-integer@")
