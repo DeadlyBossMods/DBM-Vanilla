0c7ce48cfc312a772f5fda3d6eebe109cafbccf9
MysticalOS <mysticalosx@gmail.com>
2024-04-26 13:32:50 -0400

    Fix usages of CheckDispelFilter to use type

--- DBM-Raids-Vanilla/MC/Geddon.lua
+++ DBM-Raids-Vanilla/MC/Geddon.lua
@@ -81,7 +81,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		else
 			warnBomb:Show(args.destName)
 		end
-	elseif args:IsSpell(19659) and self:CheckDispelFilter() then
+	elseif args:IsSpell(19659) and self:CheckDispelFilter("magic") then
 		specWarnIgnite:CombinedShow(0.3, args.destName)
 		specWarnIgnite:ScheduleVoice(0.3, "helpdispel")
 	end
