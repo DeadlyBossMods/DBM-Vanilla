66b66bf9f4c91f8ed947392dc040e0d122fbb0ce
MysticalOS <mysticalosx@gmail.com>
2024-04-27 07:19:34 -0400

    Fix https://github.com/DeadlyBossMods/DBM-BCVanilla/issues/138

--- DBM-Raids-BC/BlackTemple/TeronGorefiend.lua
+++ DBM-Raids-BC/BlackTemple/TeronGorefiend.lua
@@ -34,7 +34,7 @@ local specWarnDeath			= mod:NewSpecialWarningYou(40251, nil, nil, nil, 1, 2)
 local specWarnDeathEnding	= mod:NewSpecialWarningMoveAway(40251, nil, nil, nil, 3, 2)
 
 local timerCrushed			= mod:NewBuffActiveTimer(15, 40243, nil, "Healer", 2, 5, nil, DBM_COMMON_L.HEALER_ICON)
-local timerDeathCD			= mod:NewCDTimer(26.7, 40251, nil, nil, nil, 3)--32-40 (small sample size, could be bigger range)
+local timerDeathCD			= mod:NewCDTimer(24.5, 40251, nil, nil, nil, 3)--24.5-40 (small sample size, could be bigger range)
 local timerDeath			= mod:NewTargetTimer(55, 40251, nil, nil, nil, 3)
 local timerVengefulSpirit	= mod:NewTimer(60, "TimerVengefulSpirit", 40325, nil, nil, 1)
 
@@ -51,7 +51,7 @@ end
 
 function mod:OnCombatStart(delay)
 	table.wipe(CrushedTargets)
-	timerDeathCD:Start(11.1)--11-13?
+	timerDeathCD:Start(9.6)--9.6-13?
 end
 
 function mod:SPELL_AURA_APPLIED(args)
