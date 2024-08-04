c7ed6f7273176d58478ad3242176ab208d7144b2
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2024-08-04 07:39:14 -0400

    Update localization.es.lua

--- DBM-Raids-Vanilla/localization.es.lua
+++ DBM-Raids-Vanilla/localization.es.lua
@@ -691,7 +691,7 @@ L:SetMiscLocalization{
 	Breath = "%s toma aliento...",
 	YellPull = "Qué casualidad. Generalmente, debo salir de mi guarida para poder comer.",
 	YellP2 = "Este ejercicio sin sentido me aburre. ¡Os incineraré a todos desde arriba!",
-	YellP3 = "¡Parece ser que vais a necesitar otra lección, mortales!"
+	YellP3 = "¡Parece ser que vais a necesitar otra lección, mortales!",
 	SoDWarning = "Bienvenido a %s. DBM reproducirá algunos sonidos divertidos de una banda clásica legendaria durante la pelea. Puedes desactivar esto en la interfaz de usuario de DBM: escribe /dbm y navega hasta el módulo de Onyxia en Bandas -> Clásico."
 }
 
