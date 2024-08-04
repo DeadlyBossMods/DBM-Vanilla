46a0a06f2800471b1349c2593a49a3159e1228ae
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2024-08-04 07:32:44 -0400

    Update localization.es.lua

--- DBM-Raids-Vanilla/localization.es.lua
+++ DBM-Raids-Vanilla/localization.es.lua
@@ -533,6 +533,14 @@ L:SetMiscLocalization{
 	Pull		= "¡Crías imprudentes! ¡Os habéis precipitado hasta vuestra propia muerte! ¡Ahora mirad, el maestro se agita!"
 }
 
+-----------------
+--  The Molten Core (Placeholder?_  --
+-----------------
+L = DBM:GetModLocalization("MoltenCore")
+
+L:SetGeneralLocalization{
+	name = "El Núcleo de Magma"
+}
 -------------------
 --  Venoxis  --
 -------------------
@@ -684,6 +692,7 @@ L:SetMiscLocalization{
 	YellPull = "Qué casualidad. Generalmente, debo salir de mi guarida para poder comer.",
 	YellP2 = "Este ejercicio sin sentido me aburre. ¡Os incineraré a todos desde arriba!",
 	YellP3 = "¡Parece ser que vais a necesitar otra lección, mortales!"
+	SoDWarning = "Bienvenido a %s. DBM reproducirá algunos sonidos divertidos de una banda clásica legendaria durante la pelea. Puedes desactivar esto en la interfaz de usuario de DBM: escribe /dbm y navega hasta el mod de Onyxia en Bandas -> Clásico."
 }
 
 -----------------
