aabc571352d1d31ad963799055ac78994e6160cb
Paul Emmerich <tandanu@deadlybossmods.com>
2024-10-05 21:24:03 +0200

    ZG: Update Hakkar hard mode for SoD

--- DBM-Raids-Vanilla/VanillaZG/Hakkar.lua
+++ DBM-Raids-Vanilla/VanillaZG/Hakkar.lua
@@ -21,7 +21,7 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_SUCCESS 24324 24686 24687 24688 24689 24690",
-	"SPELL_AURA_APPLIED 24327 24328 24686 24687 24689 24690",
+	"SPELL_AURA_APPLIED 24327 24328 24686 24687 24689 24690 468408 468012 468491",
 	"SPELL_AURA_REMOVED 24328 24689"
 )
 
@@ -42,23 +42,46 @@ local yellBlood					= mod:NewYell(24328, nil, false, 2)
 local specWarnAspectOfThekal	= mod:NewSpecialWarningDispel(24689, "RemoveEnrage", nil, nil, 1, 6)
 
 local timerSiphon				= mod:NewNextTimer(90, 24324, nil, nil, nil, 2)
-local timerAspectOfMarli		= mod:NewTargetTimer(6, 24686, nil, nil, nil, 5)
-local timerAspectOfMarliCD		= mod:NewCDTimer(16, 24686, nil, nil, nil, 2)--16-20
-local timerAspectOfJeklik		= mod:NewTargetTimer(5, 24687, nil, false, 2, 5)--Could be spammy so off by default. Users can turn it on who want to see this
-local timerAspectOfJeklikCD		= mod:NewCDTimer(23, 24687, nil, nil, nil, 2)--23-24
-local timerAspectOfVenoxisCD	= mod:NewCDTimer(16.2, 24688, nil, nil, nil, 2)--16.2-18.3
-local timerAspectOfThekal		= mod:NewBuffActiveTimer(8, 24689, nil, "Tank|RemoveEnrage|Healer", 3, 5, nil, DBM_COMMON_L.TANK_ICON..DBM_COMMON_L.ENRAGE_ICON)
-local timerAspectOfThekalCD		= mod:NewCDTimer(15.8, 24689, nil, nil, nil, 2)
-local timerAspectOfArlokk		= mod:NewTargetTimer(2, 24690, nil, nil, nil, 2)
-local timerAspectOfArlokkCD		= mod:NewNextTimer(30, 24690, nil, nil, nil, 2)--Needs more data to verify it's a next timer, rest aren't
+local timerAspectOfMarli
+local timerAspectOfMarliCD
+local timerAspectOfJeklik
+local timerAspectOfJeklikCD
+local timerAspectOfVenoxisCD
+local timerAspectOfThekal
+local timerAspectOfThekalCD
+local timerAspectOfArlokk
+local timerAspectOfArlokkCD
+local timerNextAspect, timerSilenced
+if DBM:IsSeasonal("SeasonOfDiscovery") then
+	timerNextAspect				= mod:NewNextSpecialTimer(20, 24687)
+	timerSilenced				= mod:NewBuffFadesTimer(10, 468012)
+	timerAspectOfThekal			= mod:NewBuffActiveTimer(8, 24689, nil, "Tank|RemoveEnrage|Healer", 3, 5, nil, DBM_COMMON_L.TANK_ICON..DBM_COMMON_L.ENRAGE_ICON)
+else
+	timerAspectOfMarli			= mod:NewTargetTimer(6, 24686, nil, nil, nil, 5)
+	timerAspectOfMarliCD		= mod:NewCDTimer(16, 24686, nil, nil, nil, 2)--16-20
+	timerAspectOfJeklik			= mod:NewTargetTimer(5, 24687, nil, false, 2, 5)--Could be spammy so off by default. Users can turn it on who want to see this
+	timerAspectOfJeklikCD		= mod:NewCDTimer(23, 24687, nil, nil, nil, 2)--23-24
+	timerAspectOfVenoxisCD		= mod:NewCDTimer(16.2, 24688, nil, nil, nil, 2)--16.2-18.3
+	timerAspectOfThekal			= mod:NewBuffActiveTimer(8, 24689, nil, "Tank|RemoveEnrage|Healer", 3, 5, nil, DBM_COMMON_L.TANK_ICON..DBM_COMMON_L.ENRAGE_ICON)
+	timerAspectOfThekalCD		= mod:NewCDTimer(15.8, 24689, nil, nil, nil, 2)
+	timerAspectOfArlokk			= mod:NewTargetTimer(2, 24690, nil, nil, nil, 2)
+	timerAspectOfArlokkCD		= mod:NewNextTimer(30, 24690, nil, nil, nil, 2)--Needs more data to verify it's a next timer, rest aren't
+end
 local timerInsanity				= mod:NewTargetTimer(10, 24327, nil, nil, nil, 5)
 local timerInsanityCD			= mod:NewCDTimer(20, 24327, nil, nil, nil, 3)
 
 local enrageTimer				= mod:NewBerserkTimer(585)
 
+-- Aspects in SoD: Cast exactly every 20 seconds, order seems to be random, but he casts them all before repeating one as far as I can tell
+-- The second cycle is random again
+-- Spawn of Mar'li just randomly show up with no SPELL_SUMMON or anything, there's an emote 20 seconds later but that's too late
+-- One other aspect is also somehow missing entirely from the log, so just repeating the timer on a 20 second loop as a fallback
+
 mod:AddRangeFrameOption(10, 24328)
 
 local function IsHardMode(self)
+	-- SoD: 1832497 hp
+	local hpThreshold = DBM:IsSeasonal("SeasonOfDiscovery") and 1500000 or 1079325
 	if IsInRaid() then
 		for i = 1, GetNumGroupMembers() do
 			local UnitID = "raid"..i.."target"
@@ -66,7 +89,7 @@ local function IsHardMode(self)
 			if guid then
 				local cid = self:GetCIDFromGUID(guid)
 				if cid == 14834 then
-					if UnitHealthMax(UnitID) >= 1079325 then
+					if UnitHealthMax(UnitID) >= hpThreshold then
 						return true
 					end
 				end
@@ -79,7 +102,7 @@ local function IsHardMode(self)
 			if guid then
 				local cid = self:GetCIDFromGUID(guid)
 				if cid == 14834 then
-					if UnitHealthMax(UnitID) >= 1079325 then
+					if UnitHealthMax(UnitID) >= hpThreshold then
 						return true
 					end
 				end
@@ -90,7 +113,7 @@ local function IsHardMode(self)
 		if guid then
 			local cid = self:GetCIDFromGUID(guid)
 			if cid == 14834 then
-				if UnitHealthMax("target") >= 1079325 then
+				if UnitHealthMax("target") >= hpThreshold then
 					return true
 				end
 			end
@@ -99,19 +122,31 @@ local function IsHardMode(self)
 	return false
 end
 
+function mod:AspectTimer(delay)
+	delay = delay or 0
+	if not timerNextAspect then return end
+	timerNextAspect:Start(-delay)
+	self:UnscheduleMethod("AspectTimer")
+	self:ScheduleMethod(22 - delay, "AspectTimer", 2)
+end
+
 function mod:OnCombatStart(delay)
 	enrageTimer:Start(-delay)
 	warnSiphonSoon:Schedule(80-delay)
 	timerSiphon:Start(-delay)
 	--Hard Mode Timers
-	--This just checks if Hakkar has 1079325 health
+	--This just checks for Hakkar's health which is higher on hard mode
 	--Can't just start these on all normal mode pulls
 	if IsHardMode(self) then
-		timerAspectOfMarliCD:Start(10-delay)
-		timerAspectOfThekalCD:Start(10-delay)
-		timerAspectOfVenoxisCD:Start(14-delay)
-		timerAspectOfJeklikCD:Start(21-delay)
-		timerAspectOfArlokkCD:Start(30-delay)
+		if timerNextAspect then
+			self:AspectTimer()
+		else
+			timerAspectOfMarliCD:Start(10-delay)
+			timerAspectOfThekalCD:Start(10-delay)
+			timerAspectOfVenoxisCD:Start(14-delay)
+			timerAspectOfJeklikCD:Start(21-delay)
+			timerAspectOfArlokkCD:Start(30-delay)
+		end
 	end
 end
 
@@ -119,6 +154,7 @@ function mod:OnCombatEnd()
 	if self.Options.RangeFrame then
 		DBM.RangeCheck:Hide()
 	end
+	self:UnscheduleMethod("AspectTimer")
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
@@ -126,15 +162,15 @@ function mod:SPELL_CAST_SUCCESS(args)
 		warnSiphonSoon:Cancel()
 		warnSiphonSoon:Schedule(80)
 		timerSiphon:Start()
-	elseif args:IsSpell(24686) then
+	elseif args:IsSpell(24686) and timerAspectOfMarliCD then
 		timerAspectOfMarliCD:Start()
-	elseif args:IsSpell(24687) then
+	elseif args:IsSpell(24687) and timerAspectOfJeklikCD then
 		timerAspectOfJeklikCD:Start()
-	elseif args:IsSpell(24688) then
+	elseif args:IsSpell(24688) and timerAspectOfVenoxisCD then
 		timerAspectOfVenoxisCD:Start()
-	elseif args:IsSpell(24689) then
+	elseif args:IsSpell(24689) and timerAspectOfThekalCD then
 		timerAspectOfThekalCD:Start()
-	elseif args:IsSpell(24690) then
+	elseif args:IsSpell(24690) and timerAspectOfArlokkCD then
 		timerAspectOfArlokkCD:Start()
 	end
 end
@@ -160,7 +196,7 @@ function mod:SPELL_AURA_APPLIED(args)
 		timerAspectOfMarli:Start(args.destName)
 	elseif args:IsSpell(24687) then
 		timerAspectOfJeklik:Start(args.destName)
-	elseif args:IsSpell(24689) and args:IsDestTypeHostile() then
+	elseif (args:IsSpell(24689) or args:IsSpell(468408)) and args:IsDestTypeHostile() then
 		if self.Options.SpecWarn24689dispel then
 			specWarnAspectOfThekal:Show(args.destName)
 			specWarnAspectOfThekal:Play("enrage")
@@ -168,9 +204,15 @@ function mod:SPELL_AURA_APPLIED(args)
 			warnAspectOfThekal:Show()
 		end
 		timerAspectOfThekal:Start()
+		self:AspectTimer()
 	elseif args:IsSpell(24690) then
 		warnAspectOfArlokk:Show(args.destName)
 		timerAspectOfArlokk:Start(args.destName)
+	elseif args:IsSpell(468012) and self:AntiSpam(3, "Silence") then
+		timerSilenced:Start()
+		self:AspectTimer()
+	elseif args:IsSpell(468491) and self:AntiSpam(3, "Aoe") then
+		self:AspectTimer()
 	end
 end
 
