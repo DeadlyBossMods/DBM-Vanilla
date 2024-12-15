e97fd235f695867eabad0bd157acb4ae2abb3b3a
Paul Emmerich <tandanu@deadlybossmods.com>
2024-12-15 15:45:28 +0100

    AQ Trash: Add warning and tracking for Guardian add summon ability

--- DBM-Test-Vanilla/Season of Discovery/AQ20/Reports/Trash.lua
+++ DBM-Test-Vanilla/Season of Discovery/AQ20/Reports/Trash.lua
@@ -35,6 +35,12 @@ Timers:
 		Unknown trigger
 
 Announces:
+	Summon Anubisath Swarmguard, type=spell, spellId=17430, triggerDeltas = 113.59, 14.55, 53.48, 9.70, 16.17
+		[113.59] SPELL_SUMMON: [Anubisath Guardian->Anubisath Swarmguard: Summon Anubisath Swarmguard] Creature-0-1-509-1-15355-0000000134, Anubisath Guardian, 0xa48, Creature-0-1-509-1-15538-0000000032, Anubisath Swarmguard, 0xa48, 17430, Summon Anubisath Swarmguard, 0, 0
+			 Triggered 5x, delta times: 113.59, 14.55, 53.48, 9.70, 16.17
+	Summon Anubisath Warrior, type=spell, spellId=17431, triggerDeltas = 26.09, 53.46
+		[ 26.09] SPELL_SUMMON: [Anubisath Guardian->Anubisath Warrior: Summon Anubisath Warrior] Creature-0-1-509-1-15355-00000001A4, Anubisath Guardian, 0xa48, Creature-0-1-509-1-15537-000000000A, Anubisath Warrior, 0xa48, 17431, Summon Anubisath Warrior, 0, 0
+			 Triggered 2x, delta times: 26.09, 53.46
 	Plague on >%s<, type=target, spellId=22997, triggerDeltas = 30.44
 		[ 30.44] SPELL_AURA_APPLIED: [Anubisath Guardian->Dps6: Plague] Creature-0-1-509-1-15355-00000001A4, Anubisath Guardian, 0xa48, Player-1-00000009, Dps6, 0x512, 22997, Plague, 0, DEBUFF, 0
 
@@ -58,14 +64,18 @@ Icons:
 
 Event trace:
 	[  0.00] ADDON_LOADED: DBM-Raids-Vanilla, 0
-		RegisterEvents: Regular, SPELL_AURA_APPLIED 22997 25698 26079 1215202 1215421 2855, SPELL_PERIODIC_DAMAGE 1215421, SPELL_CAST_SUCCESS 26586, SPELL_AURA_REMOVED 22997, SPELL_MISSED, UNIT_DIED, SPELL_DAMAGE 14297 24340 8732, PLAYER_TARGET_CHANGED, NAME_PLATE_UNIT_ADDED
+		RegisterEvents: Regular, SPELL_AURA_APPLIED 22997 25698 26079 1215202 1215421 2855, SPELL_PERIODIC_DAMAGE 1215421, SPELL_CAST_SUCCESS 26586, SPELL_AURA_REMOVED 22997, SPELL_SUMMON 17430 17431, SPELL_MISSED, UNIT_DIED, SPELL_DAMAGE 14297 24340 8732, PLAYER_TARGET_CHANGED, NAME_PLATE_UNIT_ADDED
 	Unknown trigger
 		AntiSpam: NoxiousBurst
 		ShowSpecialWarning: Noxious Burst - dodge attack
 		PlaySound: VoicePack/scatter
 		StartTimer: 30.0, Noxious Burst
+	[ 26.09] SPELL_SUMMON: [Anubisath Guardian->Anubisath Warrior: Summon Anubisath Warrior] Creature-0-1-509-1-15355-00000001A4, Anubisath Guardian, 0xa48, Creature-0-1-509-1-15537-000000000A, Anubisath Warrior, 0xa48, 17431, Summon Anubisath Warrior, 0, 0
+		ShowAnnounce: Summon Anubisath Warrior
 	[ 30.44] SPELL_AURA_APPLIED: [Anubisath Guardian->Dps6: Plague] Creature-0-1-509-1-15355-00000001A4, Anubisath Guardian, 0xa48, Player-1-00000009, Dps6, 0x512, 22997, Plague, 0, DEBUFF, 0
 		ShowAnnounce: Plague on Dps6
+	[ 79.55] SPELL_SUMMON: [Anubisath Guardian->Anubisath Warrior: Summon Anubisath Warrior] Creature-0-1-509-1-15355-00000000FD, Anubisath Guardian, 0xa48, Creature-0-1-509-1-15537-0000000022, Anubisath Warrior, 0xa48, 17431, Summon Anubisath Warrior, 0, 0
+		ShowAnnounce: Summon Anubisath Warrior
 	Unknown trigger
 		AntiSpam: NoxiousBurst
 		ShowSpecialWarning: Noxious Burst - dodge attack
@@ -75,6 +85,10 @@ Event trace:
 		ShowSpecialWarning: Noxious Burst - dodge attack
 		PlaySound: VoicePack/scatter
 		StartTimer: 30.0, Noxious Burst
+	[113.59] SPELL_SUMMON: [Anubisath Guardian->Anubisath Swarmguard: Summon Anubisath Swarmguard] Creature-0-1-509-1-15355-0000000134, Anubisath Guardian, 0xa48, Creature-0-1-509-1-15538-0000000032, Anubisath Swarmguard, 0xa48, 17430, Summon Anubisath Swarmguard, 0, 0
+		ShowAnnounce: Summon Anubisath Swarmguard
+	[128.14] SPELL_SUMMON: [Anubisath Guardian->Anubisath Swarmguard: Summon Anubisath Swarmguard] Creature-0-1-509-1-15355-0000000134, Anubisath Guardian, 0xa48, Creature-0-1-509-1-15538-0000000039, Anubisath Swarmguard, 0xa48, 17430, Summon Anubisath Swarmguard, 0, 0
+		ShowAnnounce: Summon Anubisath Swarmguard
 	[134.13] SPELL_AURA_APPLIED: [Anubisath Guardian->Anubisath Guardian: Explode] Creature-0-1-509-1-15355-0000000134, Anubisath Guardian, 0xa48, Creature-0-1-509-1-15355-0000000134, Anubisath Guardian, 0xa48, 25698, Explode, 0, BUFF, 0
 		ShowSpecialWarning: Explode - run away
 		PlaySound: VoicePack/justrun
@@ -83,8 +97,15 @@ Event trace:
 		ShowSpecialWarning: Noxious Burst - dodge attack
 		PlaySound: VoicePack/scatter
 		StartTimer: 30.0, Noxious Burst
+	[181.62] SPELL_SUMMON: [Anubisath Guardian->Anubisath Swarmguard: Summon Anubisath Swarmguard] Creature-0-1-509-1-15355-0000000187, Anubisath Guardian, 0xa48, Creature-0-1-509-1-15538-0000000049, Anubisath Swarmguard, 0xa48, 17430, Summon Anubisath Swarmguard, 0, 0
+		ShowAnnounce: Summon Anubisath Swarmguard
+	[191.32] SPELL_SUMMON: [Anubisath Guardian->Anubisath Swarmguard: Summon Anubisath Swarmguard] Creature-0-1-509-1-15355-0000000187, Anubisath Guardian, 0xa48, Creature-0-1-509-1-15538-000000004E, Anubisath Swarmguard, 0xa48, 17430, Summon Anubisath Swarmguard, 0, 0
+		ShowAnnounce: Summon Anubisath Swarmguard
+	Unknown trigger
 		AntiSpam: NoxiousBurst
 		ShowSpecialWarning: Noxious Burst - dodge attack
 		PlaySound: VoicePack/scatter
 		StartTimer: 30.0, Noxious Burst
+	[207.49] SPELL_SUMMON: [Anubisath Guardian->Anubisath Swarmguard: Summon Anubisath Swarmguard] Creature-0-1-509-1-15355-0000000187, Anubisath Guardian, 0xa48, Creature-0-1-509-1-15538-0000000059, Anubisath Swarmguard, 0xa48, 17430, Summon Anubisath Swarmguard, 0, 0
+		ShowAnnounce: Summon Anubisath Swarmguard
 ]]
