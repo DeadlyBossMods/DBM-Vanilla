4ce382d4f1df9d825aac4722928e01f31db14b26
Paul Emmerich <tandanu@deadlybossmods.com>
2024-12-22 11:58:22 +0100

    Razuvious: Add var timer for shout and minor SoD update

--- DBM-Raids-Vanilla/VanillaNaxx/MilitaryQuarter/Razuvious.lua
+++ DBM-Raids-Vanilla/VanillaNaxx/MilitaryQuarter/Razuvious.lua
@@ -14,26 +14,32 @@ mod:RegisterEventsInCombat(
 	"UNIT_DIED"
 )
 
---TODO, find out if args:IsPetSource() actually works, or find something else that does
---[[
-ability.id = 29107 and type = "cast"
---]]
-local warnShoutNow		= mod:NewSpellAnnounce(29107, 1)
-local warnShoutSoon		= mod:NewSoonAnnounce(29107, 3)
+local warnShoutNow		= mod:NewSpellAnnounce(29107, 1, 6673)
+local warnShoutSoon		= mod:NewSoonAnnounce(29107, 3, 6673)
 local warnShieldWall	= mod:NewAnnounce("WarningShieldWallSoon", 3, 29061)
 
-local timerShout		= mod:NewCDTimer(25.8, 29107, nil, nil, nil, 2)-- 25.87-25.96 in classic, 16 in wrath
+local timerShout		= mod:NewCDTimer(25.8, 29107, nil, nil, nil, 2, 6673)-- 25.87-25.96 in classic, 16 in wrath
 local timerTaunt		= mod:NewCDTimer(60, 29060, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)
 local timerShieldWall	= mod:NewBuffFadesTimer(20, 29061, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)
 
 function mod:OnCombatStart(delay)
-	timerShout:Start(24 - delay)--It is 22-26 variation, but since users complained, using the median instead of the minimum
+	if DBM:IsSeasonal("SeasonOfDiscovery") then
+		-- This might also bee true for Era, old comment mentioned 22-26, but I'm pretty sure these fights were mostly unchanged on SoD
+		timerShout:Start("v25-26.34")
+	else
+		timerShout:Start("v22-26")
+	end
 	warnShoutSoon:Schedule(19 - delay)
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpell(29107) then--What does an MCed unit return?
-		timerShout:Start()
+	if args:IsSpell(29107) then
+		if DBM:IsSeasonal("SeasonOfDiscovery") then
+			-- Might be the same as the initial pull on SoD
+			timerShout:Start("v25.4-26.34")
+		else
+			timerShout:Start()
+		end
 		warnShoutNow:Show()
 		warnShoutSoon:Schedule(20)
 	elseif args:IsSpell(29060) and args:IsPetSource() then -- Taunt
