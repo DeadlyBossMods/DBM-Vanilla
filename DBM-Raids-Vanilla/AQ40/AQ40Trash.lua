1f6d6f8dceefbadafed5c5b761d7d9c539ea6a89
MysticalOS <mysticalosx@gmail.com>
2025-01-05 01:06:26 -0500

    another sync

--- DBM-Raids-Vanilla/AQ40/AQ40Trash.lua
+++ DBM-Raids-Vanilla/AQ40/AQ40Trash.lua
@@ -213,7 +213,7 @@ do
 
 	--TODO, maybe check if any bosses killed in saved lockout, in case group pulls trash after killing all required bosses
 	--Right now, it'd start a new speed run timer if you pull trash after
-	function mod:StartEngageTimers(guid, cid)
+	function mod:StartEngageTimers(guid, cid, delay)
 		if cid == 15264 or cid == 234830 then -- Anubisath Sentinel Era/SoD
 			checkFirstPull(self)
 		elseif cid == 15262 then--Obsidian Eradicator
