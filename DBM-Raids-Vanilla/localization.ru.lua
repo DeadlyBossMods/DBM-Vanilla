01fa378bf95161ae345ea5be7e40dbac2fad3cc2
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2025-01-24 01:41:57 +0500

    Update localization.ru.lua

--- DBM-Raids-Vanilla/localization.ru.lua
+++ DBM-Raids-Vanilla/localization.ru.lua
@@ -66,9 +66,8 @@ L:SetMiscLocalization{
 	Phase5 	= "выглядит готовым разлететься вдребезги",
 	Phase6 	= "Взрывается.",
 
-	HitsRemain	= "Оставшиеся попадания",
-	Frost		= "Мороз",
-	Physical	= "Физический"
+	FrostHitsPerSecond = "Попадания мороза в секунду",
+	MeleeHitsPerSecond = "Попадания в ближнем бою в секунду"
 }
 -------------
 -- Huhuran --
