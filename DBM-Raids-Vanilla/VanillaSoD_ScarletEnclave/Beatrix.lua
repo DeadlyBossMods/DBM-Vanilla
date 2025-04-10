bac40c003cdc84c5ec8f534a2e30b0399d641a13
Paul Emmerich <tandanu@deadlybossmods.com>
2025-04-11 00:20:46 +0200

    Scarlet Enclave/Beatrix: Add warning for Rose's Thorns (healer stuff)

--- DBM-Raids-Vanilla/VanillaSoD_ScarletEnclave/Beatrix.lua
+++ DBM-Raids-Vanilla/VanillaSoD_ScarletEnclave/Beatrix.lua
@@ -11,7 +11,7 @@ mod:SetEncounterID(3187)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED 1236174 1232389 1236162 1236182",
+	"SPELL_AURA_APPLIED 1236174 1232389 1236162 1236182 1232390",
 	"SPELL_AURA_REFRESH 1232389"
 )
 
@@ -20,6 +20,10 @@ mod:RegisterEventsInCombat(
 local warnSheep = mod:NewSpellAnnounce(1236174)
 local warnHolyFire = mod:NewTargetNoFilterAnnounce(1236162)
 local warnCrusaderStrike = mod:NewTargetNoFilterAnnounce(1236182)
+
+-- This seems to be a *buff* that needs to be healed to 100%
+local warnRosesThorns = mod:NewTargetNoFilterAnnounce(1232390)
+local specWarnRosesThorns = mod:NewSpecialWarningSpell(1232390, "Healer", nil, nil, 2, 8)
 
 
 -- Probably a special warning if you are targeted (as you are tanking) TBD
@@ -35,6 +39,12 @@ function mod:SPELL_AURA_APPLIED(args)
 		warnHolyFire:CombinedShow(0.1, args.destName)
 	elseif args:IsSpell(1236182) then
 		warnCrusaderStrike:CombinedShow(0.1, args.destName)
+	elseif args:IsSpell(1232390) then
+		warnRosesThorns:CombinedShow(0.2, args.destName)
+		if self:AntiSpam(5, "RosesThorns") then
+			specWarnRosesThorns:Show()
+			specWarnRosesThorns:Play("healfull")
+		end
 	end
 end
 
