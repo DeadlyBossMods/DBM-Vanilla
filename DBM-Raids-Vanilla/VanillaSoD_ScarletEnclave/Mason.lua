e3584b6a66b82d3e5dfcb2e7aa7508f63e52008b
Paul Emmerich <tandanu@deadlybossmods.com>
2025-04-16 01:22:03 +0200

    Scarlet Enclave/Mason: Show warnings at health tresholds, add timer and warning for cannon fire

--- DBM-Raids-Vanilla/VanillaSoD_ScarletEnclave/Mason.lua
+++ DBM-Raids-Vanilla/VanillaSoD_ScarletEnclave/Mason.lua
@@ -15,7 +15,9 @@ mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED 1231592 1229005",
 	"SPELL_AURA_APPLIED_DOSE 1229005",
-	"SPELL_CAST_START 1234347"
+	"SPELL_CAST_START 1234347",
+	"UNIT_HEALTH",
+	"CHAT_MSG_MONSTER_YELL"
 )
 
 -- Mortal Wound, might require a tank swap, but not sure when exactly, for now just a stack warning
@@ -24,16 +26,25 @@ local warnMortalWoundStack = mod:NewStackAnnounce(1229005)
 -- Decurse this
 local warnDrown = mod:NewSpecialWarningDispel(1231592, "RemoveCurse", nil, nil, 2, 8)
 
--- Canon Fire and Burning Oil: I don't understand these, very low damage, but can stack. Probably need a GTFO warning, but when do we trigger it?
+local timerCannons = mod:NewNextTimer(30.5, 1228376)
+local specWarnCannons = mod:NewSpecialWarningDodge(1228376, nil, nil, nil, 2, 2)
 
--- Ignite Flesh can be interrupted and is a large source of damage on the raid
+-- Ignite Flesh (cast by adds) can be interrupted, but it's hard because 1 sec cast time
 local specWarnIgnite = mod:NewSpecialWarningInterrupt(1234347, "HasInterrupt", nil, nil, 1, 2)
-local warnIgnite = mod:NewCastAnnounce(1234347, 2, nil, nil, "HasInterrupt|Healer")
+local timerIgniteCast = mod:NewCastNPTimer(1, 1234347)
+
+local warnPhase2Soon = mod:NewPrePhaseAnnounce(2)
 
 local berserkTimer = mod:NewBerserkTimer(360)
 
+local hpWarnShown1, hpWarnShown2, hpWarnShown3
+
+mod:NewGtfo{antiSpam = 3, spell = 1228509}
+
 function mod:OnCombatStart(delay)
 	berserkTimer:Start(360 - delay)
+	timerCannons:Start(30 - delay)
+	hpWarnShown1, hpWarnShown2, hpWarnShown3 = false, false, false
 end
 
 function mod:SPELL_AURA_APPLIED(args)
@@ -55,14 +66,40 @@ mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_CAST_START(args)
 	if args:IsSpell(1234347) then
-		local didShowSpecWarn = false
-		if self:CheckInterruptFilter(args.sourceGUID, false, true) then
+		if self:CheckInterruptFilter(args.sourceGUID, true, true) then
 			specWarnIgnite:Show(args.sourceName)
 			specWarnIgnite:Play("kickcast")
-			didShowSpecWarn = self.Options[specWarnIgnite.option]
 		end
-		if not didShowSpecWarn then -- A bit spammy, but only enable for interrupts and healers
-			warnIgnite:Show()
+		timerIgniteCast:Start(args.sourceGUID)
+	end
+end
+
+function mod:UNIT_HEALTH(uId)
+	if self:GetUnitCreatureId(uId) == 241021 then
+		local hp = UnitHealth(uId) / UnitHealthMax(uId)
+		if hp >= 0.76 and hp <= 0.8 and not hpWarnShown1 then
+			hpWarnShown1 = true
+			warnPhase2Soon:Show()
+		elseif hp >= 0.51 and hp <= 0.55 and not hpWarnShown2 then
+			hpWarnShown2 = true
+			warnPhase2Soon:Show()
+		elseif hp >= 0.26 and hp <= 0.3 and not hpWarnShown3 then
+			hpWarnShown3 = true
+			warnPhase2Soon:Show()
 		end
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if msg == L.Cannons1 or msg == L.Cannons2 then
+		self:SendSync("Cannons")
+	end
+end
+
+function mod:OnSync(msg)
+	if msg == "Cannons" and self:AntiSpam(5, "Cannons") then
+		timerCannons:Start()
+		specWarnCannons:Show()
+		specWarnCannons:Play("watchstep")
 	end
 end
