4c5b0ea4de3409028eca33b3be21e0592bba6d5a
Paul Emmerich <tandanu@deadlybossmods.com>
2025-04-15 18:24:53 +0200

    Scarlet Enclave/Lillian: Add icons for bombs, add exact timer for Unstable Concotion

--- DBM-Raids-Vanilla/VanillaSoD_ScarletEnclave/LillianVoss.lua
+++ DBM-Raids-Vanilla/VanillaSoD_ScarletEnclave/LillianVoss.lua
@@ -11,11 +11,12 @@ mod:SetZone(2856)
 mod:SetEncounterID(3190)
 mod:SetCreatureID(243021)
 mod:RegisterCombat("combat")
+mod:SetUsedIcons(1, 2, 3, 4, 5, 6, 7, 8)
 
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED 1233883 1232192 1233901",
+	"SPELL_AURA_APPLIED 1233883 1232192 1233901 1233849",
 	"SPELL_AURA_APPLIED_DOSE 1233883 1232192",
-	"SPELL_AURA_REMOVED 1233883 1233901",
+	"SPELL_AURA_REMOVED 1233883 1233901 1233849",
 	"SPELL_CAST_START 1233847"
 )
 
@@ -24,26 +25,56 @@ local specWarnMove		= mod:NewSpecialWarningYou(1233883, nil, nil, nil, 2, 2)
 -- Ignite goes on everyone and does a ton of damage, TODO: timer/trigger is unclear, sometimes happens after 15 sec, sometimes only after minutes
 
 -- Scarlet Grasp seems to be on a consistent timer
+-- Long cast time of 4 sec, so using a timer that indicates actual cast instead of start (similar to Harbinger in Karazhan)
 local timerScarletGrasp = mod:NewNextTimer(30.75, 1233847)
-local warnScarletGrasp = mod:NewCastAnnounce(1233847)
+local warnScarletGrasp = mod:NewSpecialWarningSoon(1233847)
 
 -- Debilitate has multiple casts and IDs, but 1232192 is the debuff that stacks
 local warnDebilitateStacks = mod:NewTargetCountAnnounce(1232192)
 local timerDebilitate = mod:NewVarTimer("v17-21", 1232192)
 
--- Enrage timer seems very short
+-- Enrage timer seems very short, but confirmed correct through multiple logs
 local berserkTimer = mod:NewBerserkTimer(180)
 
--- Poison, like Grobbulus
+-- Poison, like Grobbulus, unfortunately the timer seems to be quite random with times between 40 and 80 seconds :/
+local timerPoisonFades	= mod:NewBuffFadesTimer(8, 1233901)
 local warnPoison		= mod:NewTargetNoFilterAnnounce(1233901)
 local specWarnPoison	= mod:NewSpecialWarningYou(1233901, nil, nil, nil, 1, 2)
-local yellPoison		= mod:NewYell(1233901)
+local yellPoison		= mod:NewIconTargetYell(1233901)
+local yellPoisonFades	= mod:NewIconFadesYell(1233901)
+
+
+-- Unstable Concoction, 7 sec bomb
+local timerConcotion		= mod:NewNextTimer(30.5, 1233849)
+local timerConcotionFades	= mod:NewBuffFadesTimer(7, 1233849)
+local warnConcotion			= mod:NewTargetNoFilterAnnounce(1233849)
+local specWarnConcotion		= mod:NewSpecialWarningYou(1233849, nil, nil, 1, 2)
+local yellConcotion			= mod:NewIconTargetYell(1233849)
+local yellConcotionFades	= mod:NewIconFadesYell(1233849)
+
+mod:AddSetIconOption("SetIconOnPoisonTarget", 1233901, true, 0, {8, 7, 6, 5, 4})
+mod:AddSetIconOption("SetIconOnConcotionTarget", 1233849, true, 0, {1, 2, 3})
+
 
 mod:NewGtfo{antiSpam = 5, spell = 1234708, spellAura = 1234708, spellPeriodicDamage = 1234708}
 
+local poisonTargetIcon = 8
+local concotionTargetIcon = 3
+
+local function resetPoisonTargetIcon()
+	poisonTargetIcon = 8
+end
+
+local function resetConcotionTargetIcon()
+	concotionTargetIcon = 3
+end
+
 function mod:OnCombatStart(delay)
 	berserkTimer:Start(180 - delay)
-	timerScarletGrasp:Start(30.75 - delay)
+	timerScarletGrasp:Start(34.75 - delay)
+	timerConcotion:Start(30.5 - delay)
+	resetPoisonTargetIcon()
+	resetConcotionTargetIcon()
 end
 
 
@@ -72,28 +103,55 @@ function mod:SPELL_AURA_APPLIED(args)
 		if self:AntiSpam(2, "Debilitate") then
 			timerDebilitate:Start()
 		end
-	elseif args:IsSpell(1233901) then -- Looks like up to 5 targets at the moment --> no icon
+	elseif args:IsSpell(1233901) then
 		if args:IsPlayer() then
 			specWarnPoison:Show()
 			specWarnPoison:Play("runout")
-			yellPoison:Show()
+			yellPoison:Show(4) -- Green triangle to distinguish from the other bomb
+			yellPoisonFades:Countdown(8, 4, 4)
 		end
+		timerPoisonFades:Start()
 		warnPoison:CombinedShow(0.1, args.destName)
+		if self.Options.SetIconOnPoisonTarget then
+			self:SetIcon(args.destName, poisonTargetIcon, 8)
+			poisonTargetIcon = poisonTargetIcon - 1
+			self:Schedule(2, resetPoisonTargetIcon)
+		end
+	elseif args:IsSpell(1233849) then
+		if args:IsPlayer() then
+			specWarnConcotion:Show()
+			specWarnConcotion:Play("runout")
+			yellConcotion:Show(8)
+			yellConcotionFades:Countdown(7, 4, 8)
+		end
+		timerConcotionFades:Start()
+		timerConcotion:Start()
+		warnConcotion:CombinedShow(0.1, args.destName)
+		if self.Options.SetIconOnConcotionTarget then -- TODO: can someone get both bombs at the same time? then the icon resetting logic is messed up
+			self:SetIcon(args.destName, concotionTargetIcon, 7)
+			concotionTargetIcon = concotionTargetIcon - 1
+			self:Schedule(2, resetConcotionTargetIcon)
+		end
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 
 function mod:SPELL_AURA_REMOVED(args)
 	if args:IsSpell(1233883) and args:IsPlayer() then
-		specWarnMove:Play("safenow")
+		--specWarnMove:Play("safenow") -- TODO: does this get re-applied during some time? better not warn for now
 	elseif args:IsSpell(1233901) and args:IsPlayer() then
-		specWarnPoison:Play("safenow")
+		if not UnitIsDeadOrGhost("player") then
+			specWarnPoison:Play("safenow")
+		end
+		yellPoison:Cancel()
+	elseif args:IsSpell(1233849) and args:IsPlayer() and not UnitIsDeadOrGhost("player") then
+		specWarnConcotion:Play("safenow")
 	end
 end
 
 function mod:SPELL_CAST_START(args)
 	if args:IsSpell(1233847) then
-		timerScarletGrasp:Start()
+		timerScarletGrasp:Schedule(4, -4)
 		warnScarletGrasp:Show()
 	end
 end
