3e26883ba6fd87e6805475b4273ef96145fe06bf
Paul Emmerich <tandanu@deadlybossmods.com>
2025-04-30 23:25:37 +0200

    Scarlet Enclave/Caldoran: Add some nameplate cast timers

--- DBM-Raids-Vanilla/VanillaSoD_ScarletEnclave/Caldoran.lua
+++ DBM-Raids-Vanilla/VanillaSoD_ScarletEnclave/Caldoran.lua
@@ -14,11 +14,18 @@ mod:DisableRegenDetection()
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 1229226 1230908 1229714 1230697 1231651 1229236 1231618 1231654 1230271 1231027",
+	"SPELL_CAST_START 1229226 1230908 1229714 1230697 1231651 1229236 1231618 1231654 1230271 1231027 1229114",
 	"SPELL_CAST_SUCCESS 1230137 1230125",
 	"SPELL_AURA_APPLIED 1229272 1229503",
 	"UNIT_HEALTH"
 )
+
+-- FIXME: this should be in core
+local function castTime(spellId)
+	local timer = select(4, DBM:GetSpellInfo(spellId)) or 1000 -- GetSpellInfo takes YOUR spell haste into account...WTF?
+	local spellHaste = select(4, DBM:GetSpellInfo(10059)) / 10000 -- 10059 = Stormwind Portal, should have 10000 ms cast time
+	return timer / spellHaste / 1000
+end
 
 -- blessed feather: 5:51 1230276, 6:20 1230333 portal, 6:11 dying light 1231582, 5:59 1230268 dying light twice, 5:51 1230271 dying light again, 6:12 collapse cathedral 675770
 
@@ -41,7 +48,7 @@ local warnQuietus = mod:NewCastAnnounce(1231651)
 
 -- Blinding Flare
 local timerFlare = mod:NewVarTimer("v29-34", 1229714)
-local timerFlareCast = mod:NewCastTimer(3, 1229714)
+local timerFlareCast = mod:NewCastTimer(1229714)
 local warnFlare = mod:NewSpecialWarningSpell(1229714, nil, nil, nil, 2, 8)
 
 -- Conflagration: timer seems very random and cast often, nothing for now
@@ -63,10 +70,15 @@ local warnReclamation = mod:NewCastAnnounce(1229236)
 local warnCessation = mod:NewCastAnnounce(1230697)
 
 -- Righteous Flame: cast by adds and interruptible
-local specWarnFlame = mod:NewSpecialWarningInterrupt(1234347, nil, nil, nil, 1, 2)
+local specWarnFlame	= mod:NewSpecialWarningInterrupt(1234347, nil, nil, nil, 1, 2)
+local castNpFlame	= mod:NewCastNPTimer(castTime(1234347), 1234347)
+
+local specWarnDevotedOffering	= mod:NewSpecialWarningInterrupt(1229114, nil, nil, nil, 1, 2)
+local castNpDevotedOffering		= mod:NewCastNPTimer(castTime(1229114), 1229114)
 
 -- Judge Unworthy: cast by boss (rare, later phases only) and interruptible
-local specWarnJudge = mod:NewSpecialWarningInterrupt(1234347, nil, nil, nil, 1, 2)
+local specWarnJudge	= mod:NewSpecialWarningInterrupt(1234347, nil, nil, nil, 1, 2)
+local castNpJudge	= mod:NewCastNPTimer(castTime(1234347), 1234347)
 
 local berserkTimer = mod:NewBerserkTimer(360)
 
@@ -85,12 +97,24 @@ function mod:OnCombatStart(delay)
 end
 
 function mod:SPELL_CAST_START(args)
-	if args:IsSpell(1229226) and self:CheckInterruptFilter(args.sourceGUID, true, true) then
-		specWarnFlame:Show(args.sourceName)
-		specWarnFlame:Play("kickcast")
-	elseif args:IsSpell(1230908) and self:CheckInterruptFilter(args.sourceGUID, false, true) then
-		specWarnJudge:Show(args.sourceName)
-		specWarnJudge:Play("kickcast")
+	if args:IsSpell(1229226) then
+		if self:CheckInterruptFilter(args.sourceGUID, true, true) then
+			specWarnFlame:Show(args.sourceName)
+			specWarnFlame:Play("kickcast")
+		end
+		castNpFlame:Start(args.sourceGUID)
+	elseif args:IsSpell(1230908) then
+		if self:CheckInterruptFilter(args.sourceGUID, false, true) then
+			specWarnJudge:Show(args.sourceName)
+			specWarnJudge:Play("kickcast")
+		end
+		castNpJudge:Start(args.sourceGUID)
+	elseif args:IsSpell(1229114) then
+		if self:CheckInterruptFilter(args.sourceGUID, false, false) and self:AntiSpam(5, "Interrupt") then -- don't check for CD or can interrupt, can use stun etc here
+			specWarnDevotedOffering:Show(args.sourceName)
+			specWarnDevotedOffering:Play("kickcast")
+		end
+		castNpDevotedOffering:Start(args.sourceGUID)
 	elseif args:IsSpell(1229714) then
 		timerFlare:Start()
 		timerFlareCast:Start()
