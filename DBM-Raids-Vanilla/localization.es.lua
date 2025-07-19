fe0e79d147d21aa8ef7ed0990e89fe1cf453f3c9
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2025-07-18 22:43:05 -0400

    Update localization.es.lua

--- DBM-Raids-Vanilla/localization.es.lua
+++ DBM-Raids-Vanilla/localization.es.lua
@@ -469,6 +469,7 @@ L:SetOptionLocalization{
 	specwarnClassCall	= "Mostrar anuncio especial cuando se ve afectado por la llamada de clase"
 }
 L:SetMiscLocalization{
+	YellP1		= "¡Que comiencen los juegos!",
 	YellP2		= "Bien hecho, mis esbirros. El coraje de los mortales empieza a mermar. ¡Veamos ahora cómo se enfrentan al verdadero Señor de la Cubre de Roca Negra!",
 	YellP3		= "¡Imposible! ¡Erguíos, mis esbirros! ¡Servid a vuestro maestro una vez más!",
 	YellShaman	= "¡Chamanes, mostradme lo que pueden hacer vuestros tótems!",
