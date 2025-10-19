85ed146348c89282da02b8143eda29293f2620c3
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2025-10-19 09:07:43 -0700

    Update localization.kr.lua

--- DBM-Raids-Vanilla/localization.kr.lua
+++ DBM-Raids-Vanilla/localization.kr.lua
@@ -896,7 +896,7 @@ L:SetOptionLocalization({
 
 L:SetMiscLocalization({
 	Pull				= "죽어라, 침입자들아!",
-	AddsYell			= "일어나라, 병사들이여! 다시 일어나 싸워라!",
+	AddsYell			= "일어나라, 병사들이여! 다시 일어나 싸워라!"
 })
 
 --------------------------
@@ -1601,5 +1601,6 @@ L:SetMiscLocalization{
 	Prison = "감옥",
 	Cathedral = "대성당"
 }
+
 
 
