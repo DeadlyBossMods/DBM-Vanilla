23449fdf6fbe19739b0f07a69fbf39d76f3a9715
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2025-11-01 09:25:34 -0700

    Update localization.cn.lua

--- DBM-Raids-Vanilla/localization.cn.lua
+++ DBM-Raids-Vanilla/localization.cn.lua
@@ -54,11 +54,11 @@ L:SetOptionLocalization{
 	WarnShatter	= "提示打碎状态"
 }
 L:SetMiscLocalization{
-	Slow	= "开始减速!",
+	Slow	= "开始减速！",
 	Freezing= "冻住了",
-	Frozen	= "变成冰冻的固体!",
-	Phase4 	= "开始爆裂!",
-	Phase5 	= "看來准备好毁灭了!",
+	Frozen	= "变成冰冻的固体！",
+	Phase4 	= "开始爆裂！",
+	Phase5 	= "看來准备好毁灭了！",
 	Phase6 	= "爆炸。"
 }
 -------------
@@ -142,7 +142,14 @@ L:SetOptionLocalization{
 	WarnEmerge			= "为现身显示警告",
 	TimerEmerge			= "为现身显示计时器"
 }
+----------------
+-- AQ40 Trash --
+----------------
+L = DBM:GetModLocalization("AQ40Trash")
 
+L:SetGeneralLocalization{
+	name = "小怪"
+}
 ---------------
 -- Kurinnaxx --
 ---------------
@@ -168,12 +175,13 @@ L:SetOptionLocalization{
 }
 L:SetMiscLocalization{
 	Wave12		= "它们来了。尽量别被它们干掉，新兵。",
-	Wave3		= "我们惩罚的时刻就在眼前!让黑暗支配敌人的内心吧!",
-	Wave4		= "我们不需在被禁堵的门与石墙后等待了!我们的复仇将不再被否认!巨龙将在我们的愤怒之前颤抖!",
-	Wave5		= "恐惧是给敌人的!恐惧与死亡!",
-	Wave6		= "鹿盔将为了活命而啜泣、乞求，就像他的儿子一样!一千年的不公将在今日结束!",
-	Wave7		= "范达尔!你的时候到了!躲进翡翠梦境祈祷我们永远不会找到你吧!",
-	Wave8		= "厚颜无耻的笨蛋!我要亲手杀了你!"
+	Wave12Alt = "记得吗，拉贾克斯，上次我说会杀了你？",
+	Wave3		= "我们惩罚的时刻就在眼前！让黑暗支配敌人的内心吧！",
+	Wave4		= "我们不需在被禁堵的门与石墙后等待了！我们的复仇将不再被否认！巨龙将在我们的愤怒之前颤抖！",
+	Wave5		= "恐惧是给敌人的！恐惧与死亡！",
+	Wave6		= "鹿盔将为了活命而啜泣、乞求，就像他的儿子一样！一千年的不公将在今日结束！",
+	Wave7		= "范达尔！你的时候到了！躲进翡翠梦境祈祷我们永远不会找到你吧！",
+	Wave8		= "厚颜无耻的笨蛋！我要亲手杀了你！"
 }
 
 ----------
@@ -240,7 +248,7 @@ L:SetOptionLocalization{
 L = DBM:GetModLocalization("AQ20Trash")
 
 L:SetGeneralLocalization{
-	name = "AQ20：全程计时"
+	name = "小怪"
 }
 
 -----------------
@@ -258,8 +266,8 @@ L:SetOptionLocalization{
 	TimerAddsSpawn	= "为第一次小怪重生显示计时器"
 }
 L:SetMiscLocalization{
-	Phase2Emote	= "失去能量，停止运作!",
-	YellPull 	= "入侵者闯入孵化室了!警报!不惜一切代价保护蛋!"
+	Phase2Emote	= "失去能量，停止运作！",
+	YellPull 	= "入侵者闯入孵化室了！警报！不惜一切代价保护蛋！"
 }
 -------------------
 --  Vaelastrasz  --
@@ -271,7 +279,7 @@ L:SetGeneralLocalization{
 }
 
 L:SetMiscLocalization{
-	Event	= "太迟了，朋友! 奈法利斯的腐化已经掌握了我...我已经无法...控制我自己了。"
+	Event	= "太迟了，朋友！ 奈法利斯的腐化已经掌握了我...我已经无法...控制我自己了。"
 }
 -----------------
 --  Broodlord  --
@@ -283,7 +291,7 @@ L:SetGeneralLocalization{
 }
 
 L:SetMiscLocalization{
-	Pull	= "你怎么进来的?你们这种生物不能进来!我要毁灭你们!"
+	Pull	= "你怎么进来的?你们这种生物不能进来！我要毁灭你们！"
 }
 
 ---------------
@@ -397,16 +405,16 @@ L:SetOptionLocalization{
 	WarnPhase			= "提示阶段转换"
 }
 L:SetMiscLocalization{
-	YellP2		= "干得好，我的手下。凡人的勇气开始消退!现在，现在让我们看看他们如何应对黑石之王的力量!!!",
-	YellP3		= "不可能!出现吧，我的仆人!再次为我的主人服务!",
+	YellP2		= "干得好，我的手下。凡人的勇气开始消退！现在，现在让我们看看他们如何应对黑石之王的力量！！！",
+	YellP3		= "不可能！出现吧，我的仆人！再次为我的主人服务！",
 	YellShaman	= "萨满，让我看看",
 	YellPaladin	= "圣骑士...听说你有无数条命。让我看看到底是怎么样的吧。",
-	YellDruid	= "德鲁伊和你们愚蠢的变身术。让我们看看什么会发生吧!",
-	YellPriest	= "牧师!如果你要继续像那样治疗，我们还不如让它看起来更有趣!",
-	YellWarrior	= "战士，我知道你的力量不只如此!让我们来见识一下吧!",
-	YellRogue	= "潜行者?不要躲了，面对我吧!",
+	YellDruid	= "德鲁伊和你们愚蠢的变身术。让我们看看什么会发生吧！",
+	YellPriest	= "牧师！如果你要继续像那样治疗，我们还不如让它看起来更有趣！",
+	YellWarrior	= "战士，我知道你的力量不只如此！让我们来见识一下吧！",
+	YellRogue	= "潜行者?不要躲了，面对我吧！",
 	YellWarlock	= "术士，不要随便去玩那些你不理解的法术。看看会发生什么吧?",
-	YellHunter	= "猎人和你那讨厌的豌豆射击!",
+	YellHunter	= "猎人和你那讨厌的豌豆射击！",
 	YellMage	= "还有法师?你应该小心使用你的魔法...",
 	YellDK		= "死亡骑士士们...来这。",
 	YellMonk	= "武僧???...又是什么???"
@@ -516,8 +524,8 @@ L:SetOptionLocalization{
 	TimerEmerge			= "为现身显示计时器"
 }
 L:SetMiscLocalization{
-	Submerge	= "出现吧，我的奴仆! 保卫你们的主人!",
-	Pull		= "你这个莽撞的家伙!你简直是自寻死路!看吧，你惊动了主人!"
+	Submerge	= "出现吧，我的奴仆！ 保卫你们的主人！",
+	Pull		= "你这个莽撞的家伙！你简直是自寻死路！看吧，你惊动了主人！"
 }
 
 -----------------
@@ -528,7 +536,14 @@ L = DBM:GetModLocalization("MoltenCore")
 L:SetGeneralLocalization{
 	name = "熔火之心"
 }
+-----------------
+--  MC: Trash  --
+-----------------
+L = DBM:GetModLocalization("MCTrash")
 
+L:SetGeneralLocalization{
+	name = "小怪"
+}
 -------------------
 --  Venoxis  --
 -------------------
@@ -1039,7 +1054,15 @@ L:SetWarningLocalization({
 L:SetTimerLocalization({
 	TimerPhase2			= "第二阶段"
 })
+-----------------
+--  Naxx Trash --
+-----------------
 
+L = DBM:GetModLocalization("NaxxTrash")
+
+L:SetGeneralLocalization({
+	name = "小怪"
+})
 ---------------------------
 --  Season of Discovery  --
 ---------------------------
@@ -1278,6 +1301,7 @@ L = DBM:GetModLocalization("ShadeofEranikusSoD")
 L:SetGeneralLocalization({
 	name = "伊兰尼库斯的阴影"
 })
+
 
 
 
