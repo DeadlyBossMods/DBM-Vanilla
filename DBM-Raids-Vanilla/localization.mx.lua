f1160a552344d26ab99c5ab2f4c9a820c1b93118
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2025-11-01 15:19:15 -0700

    Update localization.mx.lua

--- DBM-Raids-Vanilla/localization.mx.lua
+++ DBM-Raids-Vanilla/localization.mx.lua
@@ -82,15 +82,10 @@ L = DBM:GetModLocalization("Ossirian")
 L:SetGeneralLocalization{
 	name 		= "Osirio el Sinmarcas"
 }
-L:SetWarningLocalization{
-	WarnVulnerable	= "%s"
-}
-L:SetTimerLocalization{
-	TimerVulnerable	= "%s"
-}
+
 L:SetOptionLocalization{
 	WarnVulnerable	= "Mostrar anuncio para debilidades",
-	TimerVulnerable	= "Mostrar temporizador para la duración de las debilidades"
+	TimerVulnerable	= "Mostrar temporizador para debilidades"
 }
 
 ----------------
