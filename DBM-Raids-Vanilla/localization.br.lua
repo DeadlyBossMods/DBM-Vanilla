af51ff573537d7ce9997fa6984fffb6c8a362d81
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2025-11-01 09:29:39 -0700

    Update localization.br.lua

--- DBM-Raids-Vanilla/localization.br.lua
+++ DBM-Raids-Vanilla/localization.br.lua
@@ -476,6 +476,8 @@ L:SetGeneralLocalization{
 ----------------
 --  Ebonroc and Flamegor  --
 ----------------
+L = DBM:GetModLocalization("EbonrocandFlamegor")
+
 L:SetGeneralLocalization{
 	name = "Petrébano e Flamagor"
 }
