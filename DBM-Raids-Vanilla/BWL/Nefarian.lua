357ebf2540035018988702fe3c6d60fd7b3f167a
anon1231823 <anon1231823@users.noreply.github.com>
2026-03-02 18:11:48 -0800

    Add warning update icon

--- DBM-Raids-Vanilla/BWL/Nefarian.lua
+++ DBM-Raids-Vanilla/BWL/Nefarian.lua
@@ -209,8 +209,8 @@ do
 				specwarnClassCall:Show()
 				specwarnClassCall:Play("targetyou")
 			else
+				warnClassCall:UpdateIcon(classIcons[arg], className)
 				warnClassCall:Show(className)
-				--warnClassCall:UpdateIcon(classIcons[arg], className)
 			end
 			timerClassCall:Start(30, className)
 			timerClassCall:UpdateIcon(classIcons[arg], className)
