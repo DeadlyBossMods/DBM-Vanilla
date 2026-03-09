22962d3eb3f6a9710142a90a9e6305e195b98d50
MysticalOS <mysticalosx@gmail.com>
2026-03-09 00:17:52 -0400

    hide sod options on non sod

--- DBM-Raids-Vanilla/BWL/Chromaggus.lua
+++ DBM-Raids-Vanilla/BWL/Chromaggus.lua
@@ -47,9 +47,6 @@ local warnPhase2Soon	= mod:NewPrePhaseAnnounce(2, 1)
 local warnPhase2		= mod:NewPhaseAnnounce(2)
 local warnMutation		= mod:NewCountAnnounce(23174, 4) ---@type Announce -- string as count in :Show() is unusual but valid
 local warnVuln			= mod:NewAnnounce("WarnVulnerable", 1, nil, nil, "WarnVulnerableNew")
-local warnRollOverSoon	= mod:NewSoonAnnounce(468199)
-local warnRollOver		= mod:NewSpellAnnounce(468199)
-local warnFetch			= mod:NewSpellAnnounce(467884)
 
 local specWarnBronze		= mod:NewSpecialWarningYou(23170, nil, nil, nil, 1, 8)
 local specWarnFrenzy		= mod:NewSpecialWarningDispel(23128, "RemoveEnrage", nil, nil, 1, 6)
@@ -60,8 +57,15 @@ local timerBreathCD		= mod:NewTimer(60, "TimerBreathCD", 23316, nil, nil, 3)
 local timerAllBreaths	= mod:NewTimer(80, "TimerAllBreaths", 23316, nil, nil, 3)
 local timerFrenzy		= mod:NewBuffActiveTimer(8, 23128, nil, "Tank|RemoveEnrage|Healer", 3, 5, nil, DBM_COMMON_L.TANK_ICON..DBM_COMMON_L.ENRAGE_ICON)
 local timerVuln			= mod:NewTimer("v16.2-25.5", "TimerVulnCD", nil, nil, nil, nil, nil, true) -- seen 16.94 - 25.53, avg 21.8; extreme outliers are somewhat rare, so going for 19.5
-local timerFetch		= mod:NewCDTimer(40, 467884)
-local timerRollOver		= mod:NewBuffActiveTimer(16, 468199)
+
+local warnRollOverSoon, warnRollOver, warnFetch, timerFetch, timerRollOver
+if DBM:IsSeasonal("SeasonOfDiscovery") then
+	warnRollOverSoon	= mod:NewSoonAnnounce(468199)
+	warnRollOver		= mod:NewSpellAnnounce(468199)
+	warnFetch			= mod:NewSpellAnnounce(467884)
+	timerFetch			= mod:NewCDTimer(40, 467884)
+	timerRollOver		= mod:NewBuffActiveTimer(16, 468199)
+end
 
 mod:AddNamePlateOption("NPAuraOnVulnerable", 22277)
 mod:AddInfoFrameOption(22277, true)
