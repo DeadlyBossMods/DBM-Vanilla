3f0c1e0af1dfe646688a84db8ed019ad28ee0bfc
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2026-03-29 10:18:43 -0700

    RemoveCurse default option for some curse spell timers

--- DBM-Raids-Vanilla/MC/Gehennas.lua
+++ DBM-Raids-Vanilla/MC/Gehennas.lua
@@ -38,7 +38,7 @@ local warnFist		= mod:NewTargetAnnounce(20277, 2, nil, false, 2)
 
 local specWarnGTFO	= mod:NewSpecialWarningGTFO(19717, nil, nil, nil, 1, 8)
 
-local timerCurseCD	= mod:NewVarTimer("v25.9-35.6", 19716, nil, nil, nil, 3, nil, DBM_COMMON_L.HEALER_ICON..DBM_COMMON_L.CURSE_ICON)
+local timerCurseCD	= mod:NewVarTimer("v25.9-35.6", 19716, nil, "RemoveCurse", nil, 3, nil, DBM_COMMON_L.HEALER_ICON..DBM_COMMON_L.CURSE_ICON)
 local timerRoF		= mod:NewCDTimer(4.8, 19717, nil, false, nil, 3)
 --local timerFist	= mod:NewBuffActiveTimer(4, 20277, nil, false, 2, 3)
 
