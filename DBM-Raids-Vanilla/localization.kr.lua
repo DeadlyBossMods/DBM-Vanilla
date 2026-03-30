7a88ca9bb4f12405b0a9bad3a778912f9d69de90
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2026-03-29 17:18:51 -0700

    indentation

--- DBM-Raids-Vanilla/localization.kr.lua
+++ DBM-Raids-Vanilla/localization.kr.lua
@@ -63,20 +63,20 @@ L:SetGeneralLocalization{
 	name = "비시두스"
 }
 L:SetWarningLocalization{
-	WarnFreeze	= "빙결 : %d/3",
-	WarnShatter	= "분해 : %d/3"
+	WarnFreeze	= "빙결: %d/3",
+	WarnShatter	= "분해: %d/3"
 }
 L:SetOptionLocalization{
 	WarnFreeze	= "빙결 상태 알림",
 	WarnShatter	= "분해 상태 알림"
 }
 L:SetMiscLocalization{
-	Slow	= "느려지기 시작했습니다!",
-	Freezing= "얼어붙고 있습니다!",
-	Frozen	= "단단하게 얼었습니다!",
-	Phase4 	= "분해되기 시작합니다!",
-	Phase5 	= "부서질 것 같습니다!",
-	Phase6 	= "폭발",
+	Slow		= "느려지기 시작했습니다!",
+	Freezing	= "얼어붙고 있습니다!",
+	Frozen		= "단단하게 얼었습니다!",
+	Phase4 		= "분해되기 시작합니다!",
+	Phase5 		= "부서질 것 같습니다!",
+	Phase6 		= "폭발",
 
 	FrostHitsPerSecond = "초당 냉기 공격 횟수",
 	MeleeHitsPerSecond = "초당 물리 공격 횟수"
