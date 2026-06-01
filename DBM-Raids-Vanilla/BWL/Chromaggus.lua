4acdd6cfb44aacab01cbf15f25a59f32adbd2ac1
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2026-05-31 20:00:41 -0700

    Update Chromaggus.lua

--- DBM-Raids-Vanilla/BWL/Chromaggus.lua
+++ DBM-Raids-Vanilla/BWL/Chromaggus.lua
@@ -184,10 +184,11 @@ function mod:OnCombatStart()
 	timerBreathCD:Start(string.format("v%s-%s", 27, 37.2), L.Breath1)
 	timerBreathCD:Start(string.format("v%s-%s", 57.3, 68.1), L.Breath2)
 	timerFrenzyCD:Start("v12.5-22.5")
+	specWarnBreathSoon:CancelVoice()
 	specWarnBreathSoon:Schedule(27) -- +2 sec casting time == you got 5 seconds to run
-	specWarnBreathSoon:ScheduleVoice(27, "breathsoon")
+	specWarnBreathSoon:ScheduleVoiceOverLap(27, "breathsoon")
 	specWarnBreathSoon:Schedule(57)
-	specWarnBreathSoon:ScheduleVoice(57, "breathsoon")
+	specWarnBreathSoon:ScheduleVoiceOverLap(57, "breathsoon")
 	mydebuffs = 0
 	if self.Options.NPAuraOnVulnerable then
 		DBM:FireEvent("BossMod_EnableHostileNameplates")
@@ -198,7 +199,7 @@ function mod:OnCombatStart()
 		timerFetch:Start(20.9)
 		timerAllBreaths:Start(40)
 		specWarnBreathSoon:Schedule(37)
-		specWarnBreathSoon:ScheduleVoice(37, "breathsoon")
+		specWarnBreathSoon:ScheduleVoiceOverLap(37, "breathsoon")
 	end
 	if DBM:IsSeasonal("SeasonOfDiscovery") then
 		self:RegisterShortTermEvents(
@@ -208,6 +209,7 @@ function mod:OnCombatStart()
 end
 
 function mod:OnCombatEnd()
+	specWarnBreathSoon:CancelVoice()
 	self:UnregisterShortTermEvents()
 	if self.Options.NPAuraOnVulnerable  then
 		DBM.Nameplate:Hide(true, nil, nil, nil, true, true)--isGUID, unit, spellId, texture, force, isHostile, isFriendly
@@ -250,7 +252,7 @@ function mod:SPELL_CAST_START(args)
 			timerBreathCD:UpdateIcon(args.spellId, args.spellName)
 			nextBreath = GetTime() + 30
 			specWarnBreathSoon:Schedule(57)
-			specWarnBreathSoon:ScheduleVoice(57, "breathsoon")
+			specWarnBreathSoon:ScheduleVoiceOverLap(57, "breathsoon")
 		else -- part of a volley
 			if volleyCount == 0 then
 				nextVolley = GetTime() + 80
