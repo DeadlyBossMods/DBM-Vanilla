a8cdbb369429b090273b0277b6e7c5455ad26ac3
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2026-07-23 21:18:56 -0700

    fix indent

--- DBM-Raids-Vanilla/VanillaNaxx/FrostwyrmLair/Kel'Thuzad.lua
+++ DBM-Raids-Vanilla/VanillaNaxx/FrostwyrmLair/Kel'Thuzad.lua
@@ -292,7 +292,7 @@ function mod:OnSync(msg, arg)
 				warnPhase:Show(DBM_CORE_L.AUTO_ANNOUNCE_TEXTS.stage:format(phase))
 			end
 			if phase == 2 then
-        		timerPhase2:Stop()
+			timerPhase2:Stop()
 				warnPhase:Play("ptwo")
 				timerFissureCD:Start("v10.4-38.4")
 				timerFrostboltCD:Start("v15.3-85.9")
@@ -302,7 +302,7 @@ function mod:OnSync(msg, arg)
 				if DBM:IsSeasonal("SeasonOfDiscovery") then
 					warnPhase:Cancel()
 					warnPhase:CancelVoice()
-				end 
+				end
 			elseif phase == 2.5 then
 				warnPhase3Soon:Show()
 			elseif phase == 3 then
