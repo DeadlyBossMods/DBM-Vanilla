8c447c0ce757ac531ced0137e240dfb8ed23f087
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2026-09-12 19:50:30 -0700

    left align icon

--- DBM-Raids-Vanilla/VanillaNaxx/MilitaryQuarter/Razuvious.lua
+++ DBM-Raids-Vanilla/VanillaNaxx/MilitaryQuarter/Razuvious.lua
@@ -67,20 +67,29 @@ do
 			local mcTimeLeft = (mindControlTimers[guid] or 0) - t
 			local exhaustionTimeLeft = (mindExhaustionTimers[guid] or 0) - t
 			local icon = mindExhaustionIcons[guid]
+			local statusIcon
+			if mcTimeLeft > 0 then
+				statusIcon = mindControlIcon
+			elseif exhaustionTimeLeft > 0 then
+				statusIcon = mindExhaustionIcon
+			end
 			local displayName = name
 			if mcOwner then
 				local color = RAID_CLASS_COLORS[DBM:GetRaidClass(mcOwner)]
 				displayName = ("|c%s%s|r"):format(color and color.colorStr or "ffffff", mcOwner)
 			end
 			displayName = icon and ("|TInterface\\TargetingFrame\\UI-RaidTargetingIcon_%d:0|t%s"):format(icon, displayName) or displayName
+			if statusIcon then
+				displayName = displayName .. ("|T%s:0|t"):format(statusIcon)
+			end
 			local key = guid .. "*" .. displayName
 			sortedLines[#sortedLines + 1] = key
 			if mindExhaustionTimers[guid] == -1 then
 				lines[key] = DEAD
 			elseif mcTimeLeft > 0 then
-				lines[key] = ("|T%s:0|t|cffff7f00%.0f|r"):format(mindControlIcon, mcTimeLeft)
+				lines[key] = ("|cffff7f00%.0f|r"):format(mcTimeLeft)
 			elseif exhaustionTimeLeft > 0 then
-				lines[key] = ("|T%s:0|t|cffff0000%.0f|r"):format(mindExhaustionIcon, exhaustionTimeLeft)
+				lines[key] = ("|cffff0000%.0f|r"):format(exhaustionTimeLeft)
 			else
 				lines[key] = ("|cff00ff00%d|r"):format(0)
 			end
