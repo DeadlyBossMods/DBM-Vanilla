ddc8f469bc893460cbfd31406320ccfa8ca91669
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2026-09-17 19:11:13 -0700

    Update Razuvious

--- DBM-Raids-Vanilla/VanillaNaxx/MilitaryQuarter/Razuvious.lua
+++ DBM-Raids-Vanilla/VanillaNaxx/MilitaryQuarter/Razuvious.lua
@@ -122,9 +122,9 @@ else
 	function mod:UNIT_AURA_UNFILTERED(unitId)
 		local guid = UnitGUID(unitId)
 		if guid and self:GetCIDFromGUID(guid) == 16803 then
-			local _, _, _, _, _, _, expirationTime = DBM:UnitDebuff(unitId, 29051)
+			local _, _, _, _, _, expirationTime = DBM:UnitDebuff(unitId, 29051)
 			local existingTimer = mindExhaustionTimers[guid]
-			if expirationTime and (not existingTimer or existingTimer ~= expirationTime) and not addDead[guid] then
+			if type(expirationTime) == "number" and expirationTime > 0 and (not existingTimer or existingTimer ~= expirationTime) and not addDead[guid] then
 				DBM:Debug(("UNIT_AURA MindExhaustion scan processed for %s: expiration=%s remaining=%s"):format(UnitName(unitId), tostring(expirationTime), ("%.1f"):format(expirationTime - GetTime())))
 				mindExhaustionTimers[guid] = expirationTime
 				timerMindExhaustionCD:Start(expirationTime - GetTime(), guid)
